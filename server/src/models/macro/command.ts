26467c0497e944ea220eba4c692dd33cf41b3292
Kyle Kemp <kyle@seiyria.com>
2020-10-29 22:39:06 -0500

    add some docs

--- server/src/models/macro/command.ts
+++ server/src/models/macro/command.ts
@@ -3,9 +3,9 @@ import { ICharacter, IMacroCommand, IMacroCommandArgs, IPlayer } from '../../int
 
 export abstract class MacroCommand implements IMacroCommand {
 
-  abstract aliases: string[];
-  canBeInstant = false;
-  canBeFast = false;
+  abstract aliases: string[];   // the aliases representing this command
+  canBeInstant = false;         // whether the command can happen immediately (ie, UI-related functions)
+  canBeFast = false;            // whether the command can happen on the 'fast' cycle (used for 'faster' commands outside the round timer)
 
   constructor(protected game: Game) {}
 
