648e42b695690eb879bfbd6433d0a4cd09b12bbe
Kyle Kemp <kyle@seiyria.com>
2020-11-06 20:32:48 -0600

    add daily reset timer

--- client/src/app/menu/menu.component.ts
+++ client/src/app/menu/menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
+import { DateTime } from 'luxon';
+import { timer } from 'rxjs';
+
 import { GameServerEvent } from '../../interfaces';
 import { Logout } from '../../stores';
 import { AssetService } from '../asset.service';
@@ -40,6 +43,7 @@ export class MenuComponent implements OnInit {
     },
     {
       name: 'Macros',
+      visibleIf: this.gameService.inGame$,
       handler: () => {}
     },
     { },
@@ -73,6 +77,14 @@ export class MenuComponent implements OnInit {
     }
   ];
 
+  // show the message or not, default no, unsaved
+  public showDailyResetMessage: boolean;
+
+  // so many props, only one output
+  public resetTimestamp: number;
+  public nowTimestamp: number;
+  public timestampDisplay: string;
+
   constructor(
     private store: Store,
     public socketService: SocketService,
@@ -84,10 +96,42 @@ export class MenuComponent implements OnInit {
     this.assetService.assetHash$.subscribe(hash => {
       this.serverAssetHash = hash;
     });
+
+    this.watchResetTime();
   }
 
   toggleMenu() {
     this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  private watchResetTime() {
+
+    const setResetTimestamp = () => {
+      let theoreticalResetTime = DateTime.fromObject({ zone: 'utc', hour: 12 });
+      if (+theoreticalResetTime < DateTime.fromObject({ zone: 'utc' })) {
+        theoreticalResetTime = theoreticalResetTime.plus({ days: 1 });
+      }
+
+      this.resetTimestamp = +theoreticalResetTime;
+    };
+
+    const formatTimestring = () => {
+      const diff = (this.resetTimestamp - this.nowTimestamp) / 1000;
+      const hours = Math.floor((diff / 60) / 60) % 60;
+      const minutes = Math.floor((diff / 60)) % 60;
+
+      this.timestampDisplay = `${hours > 0 ? hours + 'h' : ''} ${minutes}m`;
+    };
+
+    setResetTimestamp();
+    formatTimestring();
+
+    timer(0, 60000)
+      .subscribe(() => {
+        this.nowTimestamp = +DateTime.fromObject({ zone: 'utc' });
+        if (this.nowTimestamp > this.resetTimestamp) setResetTimestamp();
+        formatTimestring();
+      });
   }
 
 }
