0e44ef412ee4e9fbe8ea6aa1633ac56bd0d5ef56
Kyle Kemp <kyle@seiyria.com>
2020-11-18 21:15:55 -0600

    you can drown, but you can't die

--- server/src/helpers/game/effects/Drowning.ts
+++ server/src/helpers/game/effects/Drowning.ts
@@ -1,10 +1,21 @@
-import { ICharacter, IStatusEffect } from '../../../interfaces';
+import { DamageClass, ICharacter, IPlayer, IStatusEffect, SoundEffect } from '../../../interfaces';
 import { Effect } from '../../../models';
 
 export class Drowning extends Effect {
 
+  create(char: ICharacter, effect: IStatusEffect) {
+    effect.effectInfo.potency = (char as IPlayer).swimLevel || 4;
+  }
+
   tick(char: ICharacter, effect: IStatusEffect) {
-    console.log('drowning');
+    const hpLost = Math.floor(char.hp.maximum * (effect.effectInfo.potency / 100));
+    this.game.combatHelper.dealOnesidedDamage(char, {
+      damage: hpLost,
+      damageClass: (char as IPlayer).swimElement || DamageClass.Water,
+      damageMessage: 'You are drowning!',
+      suppressIfNegative: true,
+      overrideSfx: SoundEffect.CombatHitMelee
+    });
   }
 
 }
