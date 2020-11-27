83d037bc073302d638877c5248a8229e2b8e512a
Kyle Kemp <kyle@seiyria.com>
2020-11-27 16:54:26 -0600

    remove more todos!

--- client/src/app/services/logger.service.ts
+++ client/src/app/services/logger.service.ts
@@ -30,12 +30,10 @@ export class LoggerService {
     });
   }
 
-  // TODO: add debug flag in settings
   public debug(...data) {
     console.log(...data);
   }
 
-  // TODO: track the last X errors and display them in a modal (debug mode option)
   public error(...data) {
     console.error(...data);
 
