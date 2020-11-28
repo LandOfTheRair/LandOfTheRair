0fc77bf8f0a06391c072bf8d19b402cd5cd9021d
sarimash <brandon.stickle@gmail.com>
2020-11-28 17:08:33 -0500

    Add GMAllegiance command

---
+++ server/src/helpers/game/commands/gm/GMAllegiance.ts
@@ -0,0 +1,17 @@
+import { Allegiance, IMacroCommandArgs, IPlayer } from '../../../../interfaces';
+import { MacroCommand } from '../../../../models/macro';
+
+export class GMAllegiance extends MacroCommand {
+
+  aliases = ['@allegiance'];
+  isGMCommand = true;
+  canBeInstant = false;
+  canBeFast = false;
+
+  execute(player: IPlayer, args: IMacroCommandArgs) {
+    const allegiance = Allegiance[args.stringArgs];
+    if (!allegiance) return this.sendMessage(player, 'That is not a valid allegiance.');
+    player.allegiance = allegiance;
+    return this.sendMessage(player, `Your allegiance is now ${allegiance}`);
+  }
+}
