4933e0269faa9a3029f09400774fecda89d1cc75
sarimash <brandon.stickle@gmail.com>
2020-12-07 23:13:38 -0500

    Changes as per PR

--- server/src/helpers/game/commands/gm/GMCreateGold.ts
+++ server/src/helpers/game/commands/gm/GMCreateGold.ts
@@ -1,4 +1,4 @@
-import { IMacroCommandArgs, IPlayer } from '../../../../interfaces';
+import { IMacroCommandArgs, IPlayer, ItemSlot } from '../../../../interfaces';
 import { MacroCommand } from '../../../../models/macro';
 
 export class GMCreateGold extends MacroCommand {
@@ -9,13 +9,18 @@ export class GMCreateGold extends MacroCommand {
   canBeFast = false;
 
   execute(player: IPlayer, args: IMacroCommandArgs) {
-    const gold = this.game.itemCreator.getSimpleItem('Gold Coin');
-    const value = +args.arrayArgs[0];
-    if (!value) {
+    const value = Math.round(Math.abs(+args.arrayArgs[0]));
+    if (!value || isNaN(value) || !isFinite(value)) {
       this.sendMessage(player, `Enter an amount of gold.`);
       return;
-    } else {
-      gold.mods.value = value;
+    }
+
+    const gold = this.game.itemCreator.getGold(value);
+    if (!player.items.equipment[ItemSlot.RightHand])
+    {
+      this.game.characterHelper.setRightHand(player, gold);
+      this.sendMessage(player, `${value} gold added to your right hand.`);
+    } else {    
       this.game.worldManager.getMap(player.map).state.addItemToGround(player.x, player.y, gold);
       this.sendMessage(player, `${value} gold added to ground.`);
     }
