85f72cd7b6d7014067757bbe9c67b9f4275afd1d
Kyle Kemp <kyle@seiyria.com>
2020-12-09 15:36:01 -0600

    feat(npc): add succor tree npc

---
+++ server/src/models/world/ai/behaviors/succorer.ts
@@ -0,0 +1,56 @@
+import { random, sample } from 'lodash';
+import { Parser } from 'muud';
+
+import { Game } from '../../../../helpers';
+import { GameServerResponse, IAIBehavior, IIdentifierBehavior, INPC, IPlayer, ItemSlot } from '../../../../interfaces';
+
+export class SuccorerBehavior implements IAIBehavior {
+
+  init(game: Game, npc: INPC, parser: Parser, behavior: IIdentifierBehavior, props: any = {}) {
+
+    let { succorOz } = props;
+    succorOz ??= 1;
+
+    parser.addCommand('hello')
+      .setSyntax(['hello'])
+      .setLogic(async ({ env }) => {
+        const player = env?.player;
+        if (!player) return 'You do not exist.';
+
+        if (game.directionHelper.distFrom(player, npc) > 0) return 'You are too far away from the tree.';
+
+        env?.callbacks.emit({
+          type: GameServerResponse.SendConfirm,
+          title: `Get Succor Fruit?`,
+          content: `Would you like to take a SUCCOR fruit?`,
+          extraData: { npcSprite: npc.sprite, okText: 'Yes, take!', cancelText: 'No, not now' },
+          okAction: { command: `!privatesay`, args: `${npc.uuid}, succor` }
+        });
+
+        return `Would you like to take a SUCCOR fruit?`;
+      });
+
+    parser.addCommand('succor')
+      .setSyntax(['succor'])
+      .setLogic(async ({ env }) => {
+        const player: IPlayer = env?.player;
+        if (!player) return 'You do not exist.';
+
+        if (game.directionHelper.distFrom(player, npc) > 0) return 'You are too far away from the tree.';
+
+        const rightHand = player.items.equipment[ItemSlot.RightHand];
+        if (rightHand) return 'Your right hand cannot pick a fruit!';
+
+        const succorItem = game.itemCreator.getSimpleItem('Succor Blob');
+        succorItem.mods.destroyOnDrop = true;
+        succorItem.mods.ounces = game.subscriptionHelper.maxSuccorOz(player, succorOz);
+        succorItem.mods.succorInfo = { map: npc.map, x: npc.x, y: npc.y };
+        
+        game.characterHelper.setRightHand(player, succorItem);
+
+        return `You take a succor fruit!`;
+      });
+  }
+
+  tick() {}
+}
