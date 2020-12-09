ca3a9db2901ba7a2584e442a21612d72300ffba9
Kyle Kemp <kyle@seiyria.com>
2020-12-09 15:35:53 -0600

    feat(npc): add alchemist npc

--- server/src/models/world/ai/behaviors/alchemist.ts
+++ server/src/models/world/ai/behaviors/alchemist.ts
@@ -1,15 +1,95 @@
 import { Parser } from 'muud';
 
 import { Game } from '../../../../helpers';
-import { IAIBehavior, IAlchemistBehavior, INPC } from '../../../../interfaces';
+import { GameServerResponse, IAIBehavior, IAlchemistBehavior, IDialogChatAction, INPC, IPlayer, ItemSlot } from '../../../../interfaces';
 
 export class AlchemistBehavior implements IAIBehavior {
 
-  init(game: Game, npc: INPC, parser: Parser, behavior: IAlchemistBehavior) {
+  init(game: Game, npc: INPC, parser: Parser, behavior: IAlchemistBehavior, props: any = {}) {
+
+    let { alchCost, alchOz } = props;
+    alchCost ??= 1000;
+    alchOz ??= 10;
+
     parser.addCommand('hello')
       .setSyntax(['hello'])
-      .setLogic(async () => {
-        return `Hello, adventurer!`;
+      .setLogic(async ({ env }) => {
+        const player: IPlayer = env?.player;
+        if (!player) return 'You do not exist.';
+
+        if (game.directionHelper.distFrom(player, npc) > 2) return 'Please come closer.';
+
+        const maxOz = game.subscriptionHelper.maxSmithRepair(player, alchOz);
+
+        const message = `Hello, ${player.name}!
+        You can tell me COMBINE while holding a bottle in your right hand to
+        mix together that with other bottles of the same type in your sack.
+        I can combine up to ${maxOz} oz into one bottle. It will cost ${alchCost} gold per ounce to do this.`;
+
+        const formattedChat: IDialogChatAction = {
+          message,
+          displayNPCName: npc.name,
+          displayNPCSprite: npc.sprite,
+          displayNPCUUID: npc.uuid,
+          options: [
+            { text: 'Combine Potions', action: 'combine' },
+            { text: 'Leave', action: 'noop' },
+          ]
+        };
+
+        game.transmissionHelper.sendResponseToAccount(player.username, GameServerResponse.DialogChat, formattedChat);
+
+        return message;
+      });
+
+
+    parser.addCommand('combine')
+      .setSyntax(['combine'])
+      .setLogic(async ({ env }) => {
+        const player: IPlayer = env?.player;
+        if (!player) return 'You do not exist.';
+
+        if (game.directionHelper.distFrom(player, npc) > 2) return 'Please come closer.';
+
+        const rightHand = player.items.equipment[ItemSlot.RightHand];
+        if (!rightHand) return 'You need to hold something in your right hand!';
+
+        const maxOz = game.subscriptionHelper.maxSmithRepair(player, alchOz);
+        const { itemClass, ounces, useEffect } = game.itemHelper.getItemProperties(rightHand, ['itemClass', 'ounces', 'useEffect']);
+
+        const itemOunces = ounces ?? 0;
+
+        if (itemClass !== 'Bottle') return 'You are not holding a bottle.';
+        if (itemOunces >= maxOz) return 'That bottle is already too full for me.';
+        if (!useEffect) return 'That bottle cannot be combined!';
+
+        let itemsRemoved = 0;
+        const removeUUIDs: string[] = [];
+
+        player.items.sack.items.forEach((item) => {
+          const { useEffect: checkEffect, ounces: checkOunces } = game.itemHelper.getItemProperties(item, ['useEffect', 'ounces']);
+          const checkItemOunces = checkOunces ?? 0;
+
+          if (!checkEffect) return;
+          if (checkEffect.name !== useEffect.name) return;
+          if (checkEffect.potency !== useEffect.potency) return;
+          if ((rightHand.mods.ounces || itemOunces) + checkItemOunces > maxOz) return;
+          if (checkOunces === 0) return;
+
+          const cost = checkItemOunces * alchCost;
+          if (!game.characterHelper.hasCurrency(player, cost)) return;
+          game.characterHelper.loseCurrency(player, cost);
+
+          removeUUIDs.push(item.uuid);
+          rightHand.mods.ounces = (rightHand.mods.ounces ?? itemOunces) + checkItemOunces;
+          itemsRemoved++;
+        });
+
+        if (itemsRemoved === 0) return 'I was not able to combine any bottles.';
+
+        game.inventoryHelper.removeItemsFromSackByUUID(player, removeUUIDs);
+
+        return `I've combined ${itemsRemoved} bottles from your sack and combined them with the one in your hand, enjoy!`;
       });
   }
 
