37407599f699e22e539d5a1c28343e34d5777544
Kyle Kemp <kyle@seiyria.com>
2020-12-09 15:35:34 -0600

    feat(core): new endpoint that can be pinged by a webhook when an update is received

---
+++ server/src/http/reboot.ts
@@ -0,0 +1,28 @@
+
+import { get } from 'lodash';
+
+import { GameAction } from '../interfaces';
+
+
+export class RebootRoute {
+
+  static setup(fastify: any, { broadcast }) {
+    fastify.post('/debug/reboot', async (req) => {
+
+      if (process.env.NODE_ENV === 'production') {
+        const secret = process.env.WEBHOOK_SECRET;
+        const testSecret = get(req.body, 'hook.config.secret', '');
+
+        if (secret !== testSecret) return;
+      }
+
+      broadcast({
+        action: GameAction.ChatAddMessage,
+        timestamp: Date.now(),
+        message: 'The game just received an update and will be rebooting shortly, please exit the game to ensure your data is saved!',
+        from: '★System'
+      });
+    });
+  }
+
+}
