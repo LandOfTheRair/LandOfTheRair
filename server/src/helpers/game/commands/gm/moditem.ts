0784fa16525a64545a97e4f2c8b41fcd817ee47c
Kyle Kemp <kyle@seiyria.com>
2020-12-09 15:32:16 -0600

    feat(gm): add gm modify item

---
+++ server/src/helpers/game/commands/gm/moditem.ts
@@ -0,0 +1,27 @@
+
+import { merge } from 'lodash';
+
+import { IMacroCommandArgs, IPlayer, ItemSlot } from '../../../../interfaces';
+import { MacroCommand } from '../../../../models/macro';
+
+export class GMModItem extends MacroCommand {
+
+  aliases = ['@moditem'];
+  isGMCommand = true;
+  canBeInstant = false;
+  canBeFast = false;
+
+  execute(player: IPlayer, args: IMacroCommandArgs) {
+
+    const rightHand = player.items.equipment[ItemSlot.RightHand];
+    if (!rightHand) return this.sendMessage(player, 'You need to hold something in your right hand.');
+
+    if (!args.stringArgs) {
+      this.sendMessage(player, 'Syntax: X=Y Z=A');
+      return;
+    }
+
+    const formattedArgs = this.game.messageHelper.getMergeObjectFromArgs(args.stringArgs);
+    merge(rightHand.mods, formattedArgs);
+  }
+}
