a57178004bb8d8413bde387df82ce15920070db1
sarimash <brandon.stickle@gmail.com>
2020-12-09 22:35:55 -0500

    Convert GMGainSkill

---
+++ server/src/helpers/game/commands/gm/gainskill.ts
@@ -0,0 +1,30 @@
+import { IMacroCommandArgs, IPlayer, Skill } from '../../../../interfaces';
+import { MacroCommand } from '../../../../models/macro';
+
+export class GMGainSkill extends MacroCommand {
+
+  aliases = ['@skill'];
+  isGMCommand = true;
+  canBeInstant = false;
+  canBeFast = false;
+
+  execute(player: IPlayer, args: IMacroCommandArgs) {
+
+    if (!args.stringArgs) {
+      this.sendMessage(player, 'Syntax: SkillName Amount');
+      return;
+    }
+
+    const skill = Skill[args.stringArgs.split(' ')[0]];
+    const value = Math.round(+args.stringArgs.split(' ')[1]);
+
+    if (!skill) return this.sendMessage(player, 'That is not a valid skill name.');
+
+    try {
+      this.game.playerHelper.gainSkill(player, skill, value);
+      this.sendMessage(player, `You gained ${value} skill in ${skill}.`);
+    } catch (e) {
+      this.sendMessage(player, `@skill function failed. ${e}`);
+    }
+  }
+}
