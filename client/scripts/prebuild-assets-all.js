603c947a18c722eda0d9eb88098a7a5f40115c6b
Kyle Kemp <kyle@seiyria.com>
2020-12-11 15:53:17 -0600

    fix symlink-dir dep

--- client/scripts/prebuild-assets-all.js
+++ client/scripts/prebuild-assets-all.js
@@ -1,13 +1,14 @@
 
 const childProcess = require('child_process');
 const fs = require('fs');
-const symlinkDir = require('symlink-dir');
 
 console.log('[Client] Downloading assets (sfx, bgm, spritesheets, icons, items, npcs)...');
 
 const dl = require('download-github-repo');
 
 if(fs.existsSync('../../Content')) {  
+  const symlinkDir = require('symlink-dir');
+  
   console.log('[Client] Found Content repo, creating a symlink to it.');
 
   symlinkDir('../../Content', 'src/assets/content');
