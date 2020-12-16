86dac1d37c8f65caafa482179fd90ebc400bf967
Kyle Kemp <kyle@seiyria.com>
2020-12-16 08:33:13 -0600

    fix lint

--- server/src/helpers/game/commands/basic/prevpage.ts
+++ server/src/helpers/game/commands/basic/prevpage.ts
@@ -9,13 +9,13 @@ export class PrevPage extends MacroCommand {
   execute(player: IPlayer, args: IMacroCommandArgs) {
 
     const book = player.items.equipment[args.stringArgs];
-    if(!book) return this.sendMessage(player, 'You do not have anything in that hand!');
+    if (!book) return this.sendMessage(player, 'You do not have anything in that hand!');
 
     const { itemClass, bookCurrentPage, bookPages } = this.game.itemHelper.getItemProperties(book, ['itemClass', 'bookCurrentPage', 'bookPages']);
-    if(itemClass !== ItemClass.Book) return this.sendMessage(player, 'You are not holding a book!');
-    
+    if (itemClass !== ItemClass.Book) return this.sendMessage(player, 'You are not holding a book!');
+
     let currentPage = (bookCurrentPage ?? 0) - 1;
-    if(currentPage <= 0) currentPage = (bookPages ?? []).length - 1;
+    if (currentPage <= 0) currentPage = (bookPages ?? []).length - 1;
 
     book.mods.bookCurrentPage = currentPage;
     this.game.itemHelper.useBook(player, book, args.stringArgs as ItemSlot);
