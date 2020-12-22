51b495033a0fe50dbfe26f9db7d140e3954661cb
Kyle Kemp <kyle@seiyria.com>
2020-12-22 07:31:50 -0600

    feat(spell): add poison shell - doesnt work but will stop emailing me

--- server/src/helpers/game/effects/index.ts
+++ server/src/helpers/game/effects/index.ts
@@ -2,4 +2,5 @@
 export * from './bottle';
 export * from './npc';
 export * from './player';
+export * from './spell';
 export * from './stealth';
---
+++ server/src/helpers/game/effects/spell/debuff/Poison.ts
@@ -0,0 +1,9 @@
+import { ICharacter } from '../../../../../interfaces';
+import { Effect } from '../../../../../models';
+
+export class Poison extends Effect {
+
+  tick(char: ICharacter) {
+  }
+
+}
