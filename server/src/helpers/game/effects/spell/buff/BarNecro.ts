00eeba8b2acdf401c505ae55c1689d86d35a7ef6
Kyle Kemp <kyle@seiyria.com>
2021-03-29 08:39:05 -0500

    feat(trait): add necrotic ward

--- server/src/helpers/game/effects/spell/buff/BarNecro.ts
+++ server/src/helpers/game/effects/spell/buff/BarNecro.ts
@@ -4,6 +4,9 @@ import { Effect } from '../../../../../models';
 export class BarNecro extends Effect {
 
   public create(char: ICharacter, effect: IStatusEffect) {
+    const boost = 1 + (this.game.traitHelper.traitLevel(char, 'NecroticWard') * 0.1);
+    effect.effectInfo.potency = Math.floor(boost * effect.effectInfo.potency);
+
     effect.effectInfo.statChanges = {
       [Stat.NecroticResist]: effect.effectInfo.potency,
       [Stat.PoisonResist]: Math.floor(effect.effectInfo.potency / 5),
