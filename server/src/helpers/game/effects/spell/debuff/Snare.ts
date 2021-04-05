a89a26daf85104da67913c4e2659f3eac58c36ce
Kyle Kemp <kyle@seiyria.com>
2021-04-05 08:24:27 -0500

    feat(trait): implement roots

--- server/src/helpers/game/effects/spell/debuff/Snare.ts
+++ server/src/helpers/game/effects/spell/debuff/Snare.ts
@@ -4,7 +4,7 @@ import { Effect } from '../../../../../models';
 export class Snare extends Effect {
 
   public create(char: ICharacter, effect: IStatusEffect) {
-    effect.effectInfo.statChanges = { [Stat.Move]: -2 };
+    effect.effectInfo.statChanges = { [Stat.Move]: -effect.effectInfo.potency };
   }
 
 }
--- server/src/helpers/game/spells/debuff/Snare.ts
+++ server/src/helpers/game/spells/debuff/Snare.ts
@@ -3,6 +3,11 @@ import { Spell } from '../../../../models/world/Spell';
 
 export class Snare extends Spell {
 
+  getPotency(caster: ICharacter | null) {
+    if (!caster) return 2;
+    return this.game.traitHelper.traitLevelValue(caster, 'Roots') ? 4 : 2;
+  }
+
   getDuration(caster: ICharacter | null) {
     return caster ? Math.floor(this.game.characterHelper.getStat(caster, Stat.WIS) / 2) : 3;
   }
