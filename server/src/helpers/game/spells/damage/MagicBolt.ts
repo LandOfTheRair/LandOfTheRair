8965bb731542b45577c30d6d5f6d06a39f6fccab
Kyle Kemp <kyle@seiyria.com>
2021-04-08 09:00:34 -0500

    feat(trait): add concussive bolt

--- server/src/helpers/game/spells/damage/MagicBolt.ts
+++ server/src/helpers/game/spells/damage/MagicBolt.ts
@@ -4,6 +4,10 @@ import { Spell } from '../../../../models/world/Spell';
 export class MagicBolt extends Spell {
 
   override cast(caster: ICharacter | null, target: ICharacter | null, spellCastArgs: SpellCastArgs): void {
+
+    if (caster && target && this.game.diceRollerHelper.XInOneHundred(this.game.traitHelper.traitLevelValue(caster, 'ConcussiveBolt'))) {
+      this.game.effectHelper.addEffect(target, caster, 'Stun', { effect: { duration: 5 } });
+    }
   }
 
 }
