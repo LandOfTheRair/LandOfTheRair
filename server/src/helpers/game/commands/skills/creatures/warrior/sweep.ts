c00a56d9084de38c2f87293fac607f2f042f7e88
Kyle Kemp <kyle@seiyria.com>
2021-04-17 09:55:53 -0500

    feat(trait): add strong sweep

---
+++ server/src/helpers/game/commands/skills/creatures/warrior/sweep.ts
@@ -0,0 +1,47 @@
+
+import { sampleSize } from 'lodash';
+
+import { ICharacter, IMacroCommandArgs, IPlayer, PhysicalAttackArgs } from '../../../../../../interfaces';
+import { SpellCommand } from '../../../../../../models/macro';
+
+export class Sweep extends SpellCommand {
+
+  override aliases = ['sweep', 'art sweep'];
+  override requiresLearn = true;
+
+  override range(char: ICharacter) {
+    return 0;
+  }
+
+  override execute(player: IPlayer, args: IMacroCommandArgs) {
+    this.use(player, player, { attackRange: 0 });
+  }
+
+  override use(user: ICharacter, target: ICharacter, opts: PhysicalAttackArgs = {}): void {
+
+    const state = this.game.worldManager.getMapStateForCharacter(user);
+
+    const numTargets = 8;
+    const targets = state.getAllHostilesInRange(user, 0);
+    const foundTargets = sampleSize(targets, numTargets);
+
+    if (foundTargets.length === 0) {
+      this.sendMessage(user, 'You kick around wildly, hitting no one!');
+      return;
+    }
+
+    const damageMult = 1 + this.game.traitHelper.traitLevelValue(user, 'StrongSweep');
+
+    foundTargets.forEach(chosenTarget => {
+      this.game.combatHelper.physicalAttack(user, chosenTarget, { ...opts, isKick: true, damageMult });
+    });
+
+    const defensePenalty = 25;
+    if (defensePenalty > 0) {
+      this.game.effectHelper.addEffect(user, user, 'LoweredDefenses', { effect: { extra: { potency: defensePenalty } } });
+    }
+
+
+  }
+
+}
