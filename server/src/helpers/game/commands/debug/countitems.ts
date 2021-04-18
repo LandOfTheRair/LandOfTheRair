9257c98cb53c6e4076cf65ee82acf4e82fb943c7
wrexbe <81056464+wrexbe@users.noreply.github.com>
2021-04-17 22:17:53 -0700

    Fix count items macro

--- server/src/helpers/game/commands/debug/countitems.ts
+++ server/src/helpers/game/commands/debug/countitems.ts
@@ -8,7 +8,7 @@ export class DebugCountItems extends MacroCommand {
   override canBeFast = false;
 
   override execute(player: IPlayer, args: IMacroCommandArgs) {
-    const message = `[Debug] There are currently ${this.game.groundManager.getItemsFromGround.length} items on the ground in this world.`;
+    const message = `[Debug] There are currently ${this.game.groundManager.getAllItemsFromGround(player.map).length} items on the ground in this world.`;
     this.game.messageHelper.sendLogMessageToPlayer(player, { message, sfx: undefined }, [MessageType.Description]);
   }
 }
