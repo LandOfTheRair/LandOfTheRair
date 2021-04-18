58406b02bd655be77a33719a57f0c91268541af1
wrexbe <81056464+wrexbe@users.noreply.github.com>
2021-04-17 22:33:39 -0700

    Make line shorter for lint

--- server/src/helpers/game/commands/debug/countitems.ts
+++ server/src/helpers/game/commands/debug/countitems.ts
@@ -8,7 +8,8 @@ export class DebugCountItems extends MacroCommand {
   override canBeFast = false;
 
   override execute(player: IPlayer, args: IMacroCommandArgs) {
-    const message = `[Debug] There are currently ${this.game.groundManager.getAllItemsFromGround(player.map).length} items on the ground in this world.`;
+    const mapItemCount = this.game.groundManager.getAllItemsFromGround(player.map).length;
+    const message = `[Debug] There are currently ${mapItemCount} items on the ground in this world.`;
     this.game.messageHelper.sendLogMessageToPlayer(player, { message, sfx: undefined }, [MessageType.Description]);
   }
 }
