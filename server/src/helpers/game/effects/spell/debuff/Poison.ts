889a2e5306861906c7dacc969a24dd655203b8ce
Kyle Kemp <kyle@seiyria.com>
2021-04-21 16:14:31 -0500

    feat(trait): add corrosive poison

--- server/src/helpers/game/effects/spell/debuff/Poison.ts
+++ server/src/helpers/game/effects/spell/debuff/Poison.ts
@@ -1,7 +1,25 @@
-import { DamageClass, ICharacter, IStatusEffect } from '../../../../../interfaces';
+import { DamageClass, ICharacter, IStatusEffect, Skill, Stat } from '../../../../../interfaces';
 import { Effect } from '../../../../../models';
 
 export class Poison extends Effect {
+
+  public override create(char: ICharacter, effect: IStatusEffect) {
+
+    if (effect.sourceUUID) {
+      const mapState = this.game.worldManager.getMap(char.map)?.state;
+      const caster = mapState?.getCharacterByUUID(effect.sourceUUID);
+
+      if (caster && this.game.traitHelper.traitLevel(caster, 'CorrosivePoison')) {
+        const skill = this.game.calculatorHelper.calcSkillLevelForCharacter(caster, Skill.Restoration);
+        const reduction = (Math.floor(skill / 3) + 1);
+        effect.effectInfo.statChanges = {
+          [Stat.Mitigation]: -reduction
+        };
+
+        effect.effectInfo.tooltip = `${effect.tooltip} -${reduction} Mitigation.`;
+      }
+    }
+  }
 
   override tick(char: ICharacter, effect: IStatusEffect) {
     super.tick(char, effect);
