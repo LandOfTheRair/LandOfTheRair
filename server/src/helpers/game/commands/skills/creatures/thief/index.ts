f6b8560b67ff0c111430db8e0e811c58b0bcd84e
Kyle Kemp <kyle@seiyria.com>
2021-04-22 10:36:24 -0500

    feat(spell): add multishot

--- server/src/helpers/game/commands/skills/creatures/thief/index.ts
+++ server/src/helpers/game/commands/skills/creatures/thief/index.ts
@@ -5,5 +5,6 @@ export * from './chainkunai';
 export * from './disguise';
 export * from './disarm';
 export * from './mug';
+export * from './multishot';
 export * from './set';
 export * from './shadowmeld';
---
+++ server/src/helpers/game/commands/skills/creatures/thief/multishot.ts
@@ -0,0 +1,45 @@
+import { ICharacter, IMacroCommandArgs, IPlayer, ItemSlot } from '../../../../../../interfaces';
+import { SpellCommand } from '../../../../../../models/macro';
+
+export class Multishot extends SpellCommand {
+
+  override aliases = ['cast multishot'];
+  override requiresLearn = true;
+
+  override range(): number {
+    return 4;
+  }
+
+  override canUse(char: ICharacter, target: ICharacter): boolean {
+    const weapon = char.items.equipment[ItemSlot.RightHand];
+    const canShoot = this.game.itemHelper.getItemProperty(weapon, 'canShoot');
+    return weapon && canShoot && char.items.equipment[ItemSlot.Ammo];
+  }
+
+  override execute(player: IPlayer, args: IMacroCommandArgs) {
+    const target = this.game.targettingHelper.getFirstPossibleTargetInViewRange(player as IPlayer, args.stringArgs);
+    if (!target) return this.youDontSeeThatPerson(player as IPlayer, args.stringArgs);
+
+    if (target === player) return;
+
+    const weapon = player.items.equipment[ItemSlot.RightHand];
+    const canShoot = this.game.itemHelper.getItemProperty(weapon, 'canShoot');
+    if (!weapon || !canShoot) return this.sendMessage(player, 'You need a ranged weapon to shoot!');
+
+    if (!player.items.equipment[ItemSlot.Ammo]) return this.sendMessage(player, 'You are out of ammo!');
+
+    this.use(player, target);
+  }
+
+  override use(char: ICharacter, target: ICharacter): void {
+
+    const attacks = 2;
+    const damageMult = 0.8;
+
+    for (let i = 0; i < attacks; i++) {
+      this.game.combatHelper.physicalAttack(char, target, { damageMult, attackRange: 4 });
+    }
+
+  }
+
+}
