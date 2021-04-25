2895feeea7d7e626ad21feba176510267515863d
Kyle Kemp <kyle@seiyria.com>
2021-04-25 12:44:11 -0500

    feat(trait): added defensive voice, shielding voice, reflecting voice

--- server/src/helpers/game/effects/spell/song/index.ts
+++ server/src/helpers/game/effects/spell/song/index.ts
@@ -3,5 +3,6 @@ export * from './AriaOfRefugeSong';
 export * from './DeadlyDirgeSong';
 export * from './NightmareSerenadeSong';
 export * from './PowerBalladSong';
+export * from './Singing';
 export * from './TranquilTrillSong';
 export * from './WistfulFugueSong';
