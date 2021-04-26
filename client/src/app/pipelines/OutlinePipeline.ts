f3a5289106b1d4a20fc645d4bd62244d157dfa66
Wrexbe <wrexbe@protonmail.com>
2021-04-26 11:29:16 -0700

    Use shader for swimming

--- client/src/app/pipelines/OutlinePipeline.ts
+++ client/src/app/pipelines/OutlinePipeline.ts
@@ -1,46 +1,94 @@
+export default class OutlinePipeline extends Phaser.Renderer.WebGL.Pipelines.SinglePipeline
+{
+    constructor(game: Phaser.Types.Renderer.WebGL.WebGLPipelineConfig)
+    {
+      super({
+        game: game as any,
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        fragShader
+      });
+    }
+
+    onBind(gameObject?: Phaser.GameObjects.GameObject): void {
+      this.flush();
+      const sprite = gameObject as Phaser.GameObjects.Sprite;
+      const texture = sprite.texture.getSourceImage();
+      this.currentShader.set2f('uTextureImageSize', texture.width, texture.height);
+      this.currentShader.set2f('uTexturePixelOffset', sprite.frame.cutX, sprite.frame.cutY);
+      this.currentShader.set2f('uTextureSpriteSize', sprite.frame.cutWidth, sprite.frame.cutHeight);
+      const spriteData = sprite.pipelineData as any;
+      let outlineColor = spriteData.outlineColor as Array<number>;
+      if (!outlineColor || outlineColor.length !== 4) {
+        outlineColor = [0.0, 0.0, 0.0 ,0.0];
+      }
+
+      this.currentShader.set4fv('uOutlineColor', outlineColor);
+      if (spriteData.swimming || false) {
+        this.currentShader.set1f('uSwimming', 1.0);
+      } else {
+        this.currentShader.set1f('uSwimming', 0.0);
+      }
+      this.set1f('uTime', this.game.loop.getDuration());
+    }
+
+    // each number is a float between 0 to 1
+    public static setOutlineColorRGBA(sprite: Phaser.GameObjects.Sprite, red: number, green: number, blue: number, alpha: number) {
+      sprite.setPipelineData('outlineColor', [red, green, blue, alpha]);
+    }
+
+    // Color is an array containing [red, green, blue, alpha], each number is a float between 0 to 1
+    public static setOutlineColor(sprite: Phaser.GameObjects.Sprite, color: Array<number>) {
+      sprite.setPipelineData('outlineColor', color);
+    }
+
+    // Color is an array containing [red, green, blue, alpha], each number is a float between 0 to 1
+    public static setSwimming(sprite: Phaser.GameObjects.Sprite, swimming: boolean) {
+      sprite.setPipelineData('swimming', swimming);
+    }
+}
+
 const fragShader = `
-precision highp float;
+precision lowp float;
 uniform sampler2D uMainSampler;
 varying vec2 outTexCoord;
-uniform vec2 uTextureSize;
+uniform vec2 uTexturePixelOffset;
+uniform vec2 uTextureImageSize;
+uniform vec4 uOutlineColor;
+uniform vec2 uTextureSpriteSize;
+uniform bool uSwimming;
+uniform float uTime;
 
 void outline( out vec4 fragColor)
 {
+  vec2 pixelSize = vec2(1.0,1.0)/uTextureImageSize;
+  vec2 spritePixelSmooth = (outTexCoord/pixelSize) - uTexturePixelOffset;
+  vec2 spritePixel = floor(spritePixelSmooth);
   vec4 color = texture2D(uMainSampler, outTexCoord);
-  vec2 onePixel = vec2(1.0, 1.0) / uTextureSize;
-  vec4 colorU = texture2D(uMainSampler, vec2(outTexCoord.x, outTexCoord.y - onePixel.y));
-  vec4 colorD = texture2D(uMainSampler, vec2(outTexCoord.x, outTexCoord.y + onePixel.y));
-  vec4 colorL = texture2D(uMainSampler, vec2(outTexCoord.x + onePixel.x, outTexCoord.y));
-  vec4 colorR = texture2D(uMainSampler, vec2(outTexCoord.x - onePixel.x, outTexCoord.y));
-  vec2 spritesheetPixel = outTexCoord / onePixel;
-  vec2 spritePixel = vec2(floor(mod(spritesheetPixel.x, 64.0)),floor(mod(spritesheetPixel.y, 64.0)));
-  if (spritePixel.x == 0.0) colorL.a = 0.0;
-  if (spritePixel.x == 63.0) colorR.a = 0.0;
-  if (spritePixel.y == 0.0) colorU.a = 0.0;
-  if (spritePixel.y == 63.0) colorD.a = 0.0;
-  
   fragColor = color;
+  bool edgeL = spritePixel.x == 0.0;
+  bool edgeR = spritePixel.x == uTextureSpriteSize.x - 1.0;
+  bool edgeU = spritePixel.y == 0.0;
+  bool edgeD = spritePixel.y == uTextureSpriteSize.y - 1.0;
+  if (uOutlineColor.a != 0.0) {
+    if (color.a == 0.0) {
+      bool colorU = texture2D(uMainSampler, outTexCoord - vec2(0, pixelSize.y)).a == 1.0 && !edgeU;
+      bool colorD = texture2D(uMainSampler, outTexCoord + vec2(0, pixelSize.y)).a == 1.0 && !edgeD;
+      bool colorL = texture2D(uMainSampler, outTexCoord - vec2(pixelSize.x, 0)).a == 1.0 && !edgeL;
+      bool colorR = texture2D(uMainSampler, outTexCoord + vec2(pixelSize.x, 0)).a == 1.0 && !edgeR;
+      if ( colorU || colorD || colorL || colorR)
+      {
+        fragColor = uOutlineColor;
+      }
+    } else if (color.a == 1.0 && (edgeL || edgeR || edgeU || edgeD)) {
+      fragColor = uOutlineColor;
+    }
+  }
   
-  if (color.a == 1.0 && (colorU.a == 0.0 || colorD.a == 0.0 || colorL.a == 0.0 || colorR.a == 0.0)) {
-    fragColor = vec4(1.0, 0.0, 0.0, .2);
+  if (uSwimming && spritePixelSmooth.y > (cos(uTime*2.5)*2.0)+32.0) {
+    fragColor.a = 0.001;
   }
 }
 
 void main(void) {
   outline(gl_FragColor);
 }`;
-
-export default class OutlinePipeline extends Phaser.Renderer.WebGL.Pipelines.SinglePipeline
-{
-    constructor(game)
-    {
-        super({
-            game,
-            fragShader,
-            uniforms: [
-                'uMainSampler',
-                'uTextureSize',
-            ],
-        } as any);
-    }
-}
