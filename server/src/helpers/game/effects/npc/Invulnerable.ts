937226a9c801a7eaae436b0e3a9d8b5d2249023d
Kyle Kemp <kyle@seiyria.com>
2021-04-28 08:54:04 -0500

    feat(npc): added invulnerable npc effect

---
+++ server/src/helpers/game/effects/npc/Invulnerable.ts
@@ -0,0 +1,11 @@
+
+
+import { Effect } from '../../../../models';
+
+export class Invulnerable extends Effect {
+
+  public override incoming(): number {
+    return 0;
+  }
+
+}
--- server/src/helpers/game/effects/npc/index.ts
+++ server/src/helpers/game/effects/npc/index.ts
@@ -2,4 +2,5 @@
 export * from './AcolyteOverseer';
 export * from './Attribute';
 export * from './Dangerous';
+export * from './Invulnerable';
 export * from './Mood';
