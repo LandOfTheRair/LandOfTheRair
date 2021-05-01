16288aaa28b9aff3d493e70b07496a47232b5f11
Kyle Kemp <kyle@seiyria.com>
2021-05-01 10:34:41 -0500

    feat(npc): add item mod npc

--- server/src/models/world/ai/behaviors/itemmodder.ts
+++ server/src/models/world/ai/behaviors/itemmodder.ts
@@ -1,7 +1,7 @@
 import { Parser } from 'muud';
 
 import { Game } from '../../../../helpers';
-import { IAIBehavior, IItemModderBehavior, INPC } from '../../../../interfaces';
+import { GameServerResponse, IAIBehavior, IDialogChatAction, IItemModderBehavior, INPC, ItemSlot } from '../../../../interfaces';
 
 export class ItemModderBehavior implements IAIBehavior {
 
@@ -15,7 +15,66 @@ export class ItemModderBehavior implements IAIBehavior {
 
         if (game.directionHelper.distFrom(player, npc) > 0) return 'Please come closer.';
 
-        return 'Hello!';
+        const message = `Greetings, ${player.name}! I am he who can tweak your items.
+        Just tell me one of these things and I can tell you more: PRONE.`;
+
+        const formattedChat: IDialogChatAction = {
+          message,
+          displayTitle: npc.name,
+          displayNPCName: npc.name,
+          displayNPCSprite: npc.sprite,
+          displayNPCUUID: npc.uuid,
+          options: [
+            { text: 'Prone?', action: 'prone' },
+            { text: 'Leave', action: 'noop' },
+          ]
+        };
+
+        game.transmissionHelper.sendResponseToAccount(player.username, GameServerResponse.DialogChat, formattedChat);
+
+        return message;
+      });
+
+    parser.addCommand('prone')
+      .setSyntax(['prone'])
+      .setLogic(async ({ env }) => {
+        const player = env?.player;
+        if (!player) return 'You do not exist.';
+
+        if (game.directionHelper.distFrom(player, npc) > 0) return 'Please come closer.';
+
+        const rightHand = player.items.equipment[ItemSlot.RightHand];
+        const leftHand = player.items.equipment[ItemSlot.LeftHand];
+
+        if (rightHand && leftHand && rightHand.name === leftHand.name) {
+          if (!game.itemHelper.isOwnedBy(player, leftHand)
+          || !game.itemHelper.isOwnedBy(player, rightHand)) return 'You must own both of those items!';
+
+          const proneChance = game.itemHelper.getItemProperty(rightHand, 'proneChance');
+          if (!proneChance || proneChance <= 0) return 'That item does not prone!';
+
+          rightHand.mods.proneChance = 0;
+          game.characterHelper.setLeftHand(player, undefined);
+          return 'Done! No more proning.';
+        }
+
+        const message = `Yes! I can remove proning from your item. Bring me two of the same weapon;
+        I'll take the one in your left hand, and remove proning from the right.`;
+
+        const formattedChat: IDialogChatAction = {
+          message,
+          displayTitle: npc.name,
+          displayNPCName: npc.name,
+          displayNPCSprite: npc.sprite,
+          displayNPCUUID: npc.uuid,
+          options: [
+            { text: 'Got it', action: 'noop' },
+          ]
+        };
+
+        game.transmissionHelper.sendResponseToAccount(player.username, GameServerResponse.DialogChat, formattedChat);
+
+        return message;
       });
   }
 
