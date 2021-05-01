fe3d6c29313aa9dd4bd81a4306512f8661f0a6cc
Kyle Kemp <kyle@seiyria.com>
2021-05-01 09:51:11 -0500

    feat(npc): add buffer npc

--- server/src/models/world/ai/behaviors/buffer.ts
+++ server/src/models/world/ai/behaviors/buffer.ts
@@ -1,7 +1,7 @@
 import { Parser } from 'muud';
 
 import { Game } from '../../../../helpers';
-import { IAIBehavior, IBufferBehavior, INPC } from '../../../../interfaces';
+import { GameServerResponse, IAIBehavior, IBufferBehavior, IDialogChatAction, INPC } from '../../../../interfaces';
 
 export class BufferBehavior implements IAIBehavior {
 
@@ -15,7 +15,51 @@ export class BufferBehavior implements IAIBehavior {
 
         if (game.directionHelper.distFrom(player, npc) > 0) return 'Please come closer.';
 
+        const message = `Greetings, ${player.name}! It is I who can grant unto thee some temporary relief.
+        Just tell me one of these spells, and I'll fix you right up: INVISIBILITY, DARKVISION.`;
+
+        const formattedChat: IDialogChatAction = {
+          message,
+          displayTitle: npc.name,
+          displayNPCName: npc.name,
+          displayNPCSprite: npc.sprite,
+          displayNPCUUID: npc.uuid,
+          options: [
+            { text: 'Invisibility', action: 'invisibility' },
+            { text: 'DarkVision', action: 'darkvision' },
+            { text: 'Leave', action: 'noop' },
+          ]
+        };
+
+        game.transmissionHelper.sendResponseToAccount(player.username, GameServerResponse.DialogChat, formattedChat);
+
         return 'Hello!';
+      });
+
+    parser.addCommand('darkvision')
+      .setSyntax(['darkvision'])
+      .setLogic(async ({ env }) => {
+        const player = env?.player;
+        if (!player) return 'You do not exist.';
+
+        if (game.directionHelper.distFrom(player, npc) > 0) return 'Please come closer.';
+
+        game.effectHelper.addEffect(player, npc, 'DarkVision', { effect: { duration: 900 } });
+
+        return 'There you go!';
+      });
+
+    parser.addCommand('invisibility')
+      .setSyntax(['invisibility'])
+      .setLogic(async ({ env }) => {
+        const player = env?.player;
+        if (!player) return 'You do not exist.';
+
+        if (game.directionHelper.distFrom(player, npc) > 0) return 'Please come closer.';
+
+        game.effectHelper.addEffect(player, npc, 'Invisibility', { effect: { duration: 900 } });
+
+        return 'There you go!';
       });
   }
 
