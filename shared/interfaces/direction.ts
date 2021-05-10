574a3ce58bb71a5b136ac40e548498afd510e123
Wrexbe <wrexbe@protonmail.com>
2021-05-09 22:03:14 -0700

    Add shared direction tests, and direction bug

--- shared/interfaces/direction.ts
+++ shared/interfaces/direction.ts
@@ -3,13 +3,13 @@
  * Direction represents one, or more directions (a single, or multi direction)
  */
 export enum Direction {
-  Center    = 0b000_0_0_000,
-  Northwest = 0b100_0_0_000,
-  North     = 0b010_0_0_000,
-  Northeast = 0b001_0_0_000,
-  West      = 0b000_1_0_000,
-  East      = 0b000_0_1_000,
-  Southwest = 0b000_0_0_100,
-  South     = 0b000_0_0_010,
-  Southeast = 0b000_0_0_001,
+  Center    = 0b000_0_0_000,// 0
+  Northwest = 0b100_0_0_000,// 128
+  North     = 0b010_0_0_000,// 64
+  Northeast = 0b001_0_0_000,// 32
+  West      = 0b000_1_0_000,// 16
+  East      = 0b000_0_1_000,// 8
+  Southwest = 0b000_0_0_100,// 4
+  South     = 0b000_0_0_010,// 2
+  Southeast = 0b000_0_0_001,// 1
 }
