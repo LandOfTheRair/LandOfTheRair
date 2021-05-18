d525c8797d674148d723c3de55838ac94bb1a1ef
Kyle Kemp <kyle@seiyria.com>
2021-05-18 09:42:54 -0500

    feat(trait): add effective potions and ancient potions

--- server/src/helpers/game/effects/bottle/ExactHeal.ts
+++ server/src/helpers/game/effects/bottle/ExactHeal.ts
@@ -4,7 +4,9 @@ import { Effect } from '../../../../models';
 export class ExactHeal extends Effect {
 
   override apply(char: ICharacter, effect: IStatusEffect) {
-    this.game.characterHelper.heal(char, effect.effectInfo.potency);
+    const bonus = this.game.traitHelper.traitLevelValue(char, 'EffectivePotions')
+                + this.game.traitHelper.traitLevelValue(char, 'AncientPotions');
+    this.game.characterHelper.heal(char, effect.effectInfo.potency + bonus);
     this.sendMessage(char, { message: 'You\'ve been healed.' });
   }
 
