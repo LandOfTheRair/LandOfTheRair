f3d3b13f38c7142353eb6db7faaf823f771956d0
Kyle Kemp <kyle@seiyria.com>
2021-06-03 10:00:48 -0500

    fix(spell): slight nerf to burning

--- server/src/helpers/game/effects/spell/debuff/Burning.ts
+++ server/src/helpers/game/effects/spell/debuff/Burning.ts
@@ -7,7 +7,7 @@ export class Burning extends Effect {
     super.tick(char, effect);
 
     this.game.combatHelper.magicalAttack(null, char, {
-      damage: effect.effectInfo.potency / 20,
+      damage: effect.effectInfo.potency / 40,
       damageClass: DamageClass.Fire,
       defMsg: 'You are burning!'
     });
