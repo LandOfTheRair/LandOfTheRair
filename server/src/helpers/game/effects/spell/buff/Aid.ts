f9a7517a4da368dc70c5010f15edf58083649837
Kyle Kemp <kyle@seiyria.com>
2021-06-15 09:01:05 -0500

    fix(spell): aid scales based on potency instead of skill

--- server/src/helpers/game/effects/spell/buff/Aid.ts
+++ server/src/helpers/game/effects/spell/buff/Aid.ts
@@ -1,15 +1,20 @@
-import { ICharacter, IStatusEffect, Skill, Stat } from '../../../../../interfaces';
+import { ICharacter, IStatusEffect, Stat } from '../../../../../interfaces';
 import { Effect } from '../../../../../models';
 
 export class Aid extends Effect {
 
   public override create(char: ICharacter, effect: IStatusEffect) {
-    const skill = this.game.characterHelper.getSkillLevel(char, Skill.Restoration) + 1;
+    const potency = effect.effectInfo.potency ?? 0;
+
+    const dexBoost = Math.max(1, Math.floor(potency / 5));
+    const offenseBoost = potency;
 
     effect.effectInfo.statChanges = {
-      [Stat.DEX]: Math.max(1, Math.floor(skill / 5)),
-      [Stat.Offense]: skill
+      [Stat.DEX]: dexBoost,
+      [Stat.Offense]: offenseBoost
     };
+
+    effect.effectInfo.tooltip = `Increase DEX by ${dexBoost} and Offense by ${offenseBoost}.`;
   }
 
 }
