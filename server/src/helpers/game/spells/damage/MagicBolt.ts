2d01a6820f523aa438c95d697652dbe076a0c467
Kyle Kemp <kyle@seiyria.com>
2022-02-06 14:38:09 -0600

    fix(spell): prevent concussive bolt specifically from recasting stun

--- server/src/helpers/game/spells/damage/MagicBolt.ts
+++ server/src/helpers/game/spells/damage/MagicBolt.ts
@@ -5,7 +5,9 @@ export class MagicBolt extends Spell {
 
   override cast(caster: ICharacter | null, target: ICharacter | null, spellCastArgs: SpellCastArgs): void {
 
-    if (caster && target && this.game.traitHelper.rollTraitValue(caster, 'ConcussiveBolt')) {
+    if (caster && target
+    && this.game.traitHelper.rollTraitValue(caster, 'ConcussiveBolt')
+    && !this.game.effectHelper.hasEffect(target, 'Stun')) {
       this.game.effectHelper.addEffect(target, caster, 'Stun', {
         effect: {
           duration: 5,
