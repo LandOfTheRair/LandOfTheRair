7bada44227266b98fdf65e0fd538826e91dd9a9f
Kyle Kemp <kyle@seiyria.com>
2022-02-06 21:08:46 -0600

    add file

---
+++ server/src/helpers/game/commands/gm/freezeai.ts
@@ -0,0 +1,15 @@
+import {  IPlayer } from '../../../../interfaces';
+import { MacroCommand } from '../../../../models/macro';
+
+export class GMFreezeAI extends MacroCommand {
+
+  override aliases = ['@freezeai', '@fa'];
+  override isGMCommand = true;
+  override canBeInstant = false;
+  override canBeFast = false;
+
+  override execute(player: IPlayer) {
+    this.game.configManager.toggleAIFreeze();
+    this.sendMessage(player, 'AI is now ' + (this.game.configManager.isAIActive ? 'active' : 'frozen') + '.');
+  }
+}
