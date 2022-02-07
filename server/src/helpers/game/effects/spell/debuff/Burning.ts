5c5d58445deb18ef9f6204308dba399194f687f0
Kyle Kemp <kyle@seiyria.com>
2022-02-06 20:06:26 -0600

    fix(spell): burning will check for a caster if possible

--- server/src/helpers/game/effects/spell/debuff/Burning.ts
+++ server/src/helpers/game/effects/spell/debuff/Burning.ts
@@ -6,7 +6,13 @@ export class Burning extends Effect {
   override tick(char: ICharacter, effect: IStatusEffect) {
     super.tick(char, effect);
 
-    this.game.combatHelper.magicalAttack(null, char, {
+    let caster: ICharacter | null = null;
+    if (effect.sourceUUID) {
+      const mapState = this.game.worldManager.getMap(char.map)?.state;
+      caster = mapState?.getCharacterByUUID(effect.sourceUUID) ?? null;
+    }
+
+    this.game.combatHelper.magicalAttack(caster, char, {
       damage: effect.effectInfo.potency / 40,
       damageClass: DamageClass.Fire,
       defMsg: 'You are burning!'
