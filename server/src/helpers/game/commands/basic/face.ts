0356610af9a6a9c7ac1abc7ddd4f6aa056af6b50
Kyle Kemp <kyle@seiyria.com>
2022-02-23 10:25:12 -0600

    actually add the face command smh

---
+++ server/src/helpers/game/commands/basic/face.ts
@@ -0,0 +1,20 @@
+import { Direction, directionFromText, GameAction, IMacroCommandArgs, IPlayer } from '../../../../interfaces';
+import { MacroCommand } from '../../../../models/macro';
+
+export class Face extends MacroCommand {
+
+  override aliases = ['face'];
+  override canBeInstant = false;
+  override canBeFast = true;
+
+  override execute(player: IPlayer, args: IMacroCommandArgs) {
+    const dir = args.stringArgs;
+    if (!['n', 'w', 'e', 's'].includes(dir.toLowerCase())) return this.sendMessage(player, 'Invalid direction.');
+
+    player.dir = directionFromText(dir.toUpperCase()) as Direction;
+
+    this.game.transmissionHelper.sendActionToAccount(player.username, GameAction.GamePatchPlayer, {
+      player: { dir: player.dir }
+    });
+  }
+}
