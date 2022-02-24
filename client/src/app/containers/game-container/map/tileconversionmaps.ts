e2015ea68905c0f95a09d27e5bafe366bcf53d30
Kyle Kemp <kyle@seiyria.com>
2022-02-24 16:24:36 -0600

    fix(ui): move tile conversion maps to content and out of hardcoded in client

--- client/src/app/containers/game-container/map/tileconversionmaps.ts
+++ client/src/app/containers/game-container/map/tileconversionmaps.ts
@@ -3,67 +3,10 @@
 // 2399 - horizontal cave wall
 
 import { invert } from 'lodash';
+import tileConversionMaps from '../../../../assets/content/_output/tileconversionmaps.json';
 
-export const VerticalDoorGids = {
+export const VerticalDoorGids = tileConversionMaps.verticalDoorGids;
 
-  // blue wall door vert
-  1058: true,
-
-  // undead wall door vert
-  1065: true,
-
-  // cave wall door vert
-  1070: true,
-
-  // stone wall door vert
-  1077: true,
-
-  // green wall door vert
-  1088: true,
-
-  // town door vert
-  1095: true,
-
-  // town2 door vert
-  1116: true,
-
-  // town3 door vert
-  1123: true,
-
-  // cave green wall door vert
-  1211: true
-
-};
-
-export const TrueSightMap = {
-
-  // blue wall horiz, blue wall vert
-  14: 1064,
-  15: 1065,
-
-  // cave horiz, cave vert
-  62: 1076,
-  63: 1075,
-
-  // stone horiz, stone vert
-  78: 1087,
-  79: 1086,
-
-  // green stone horiz, green stone vert
-  94: 1094,
-  95: 1093,
-
-  // town horiz, town vert
-  111: 1100,
-  112: 1101,
-
-  // town2 horiz, town2 vert
-  175: 1123,
-  176: 1124,
-
-  // green cave wall horiz, green cave wall vert
-  350: 1217,
-  351: 1216
-};
+export const TrueSightMap = tileConversionMaps.truesightMap;
 
 export const TrueSightMapReversed = invert(TrueSightMap);
