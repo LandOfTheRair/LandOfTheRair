c5033d3a4df7969b90a93ec03588b165e32ccd27
Kyle Kemp <kyle@seiyria.com>
2022-03-16 09:17:58 -0500

    feat(spell): conjure weapon is now more flexible

--- server/src/helpers/game/commands/skills/creatures/utility/conjuresword.ts
+++ server/src/helpers/game/commands/skills/creatures/utility/conjuresword.ts
@@ -1,4 +1,4 @@
-import { ICharacter, IMacroCommandArgs, IPlayer, ItemSlot } from '../../../../../../interfaces';
+import { GameServerResponse, ICharacter, IDialogChatAction, IMacroCommandArgs, IPlayer, ItemClass, ItemSlot, WeaponClass, WeaponClasses } from '../../../../../../interfaces';
 import { SpellCommand } from '../../../../../../models/macro';
 
 export class ConjureSword extends SpellCommand {
@@ -14,6 +14,38 @@ export class ConjureSword extends SpellCommand {
   }
 
   override execute(player: IPlayer, args: IMacroCommandArgs) {
+
+    if (!args.stringArgs) {
+
+      const options = [
+        { text: 'Nevermind', action: 'noop' }
+      ];
+
+      let msg = 'Choose a weapon:';
+
+      const weaponChoices = this.game.contentManager.getItemsMatchingName('Conjured');
+      weaponChoices.forEach((weapon, i) => {
+        if (!WeaponClasses.includes(weapon.itemClass as WeaponClass)) return;
+        if (weapon.itemClass === ItemClass.Shield) return;
+
+        const weaponName = weapon.name.split(' ')[1];
+        msg = `${msg}<br>${i + 1}: ${weaponName}`;
+        options.push({ text: `${weaponName}`, action: `cast conjuresword ${weaponName}` });
+      });
+
+      this.game.messageHelper.sendLogMessageToPlayer(player, { message: msg });
+
+      const formattedChat: IDialogChatAction = {
+        message: 'Create which weapon?',
+        displayTitle: 'Conjure Weapon',
+        options
+      };
+
+      this.game.transmissionHelper.sendResponseToAccount(player.username, GameServerResponse.DialogChat, formattedChat);
+
+      return;
+    }
+
     this.castSpellAt(player, player, args);
   }
 
--- server/src/helpers/game/spells/utility/ConjureSword.ts
+++ server/src/helpers/game/spells/utility/ConjureSword.ts
@@ -1,4 +1,4 @@
-import { ICharacter, IPlayer, ItemSlot, Skill, SpellCastArgs, Stat } from '../../../../interfaces';
+import { ICharacter, IPlayer, ItemClass, ItemSlot, Skill, SpellCastArgs, Stat, WeaponClass, WeaponClasses } from '../../../../interfaces';
 import { Spell } from '../../../../models/world/Spell';
 
 export class ConjureSword extends Spell {
@@ -14,21 +14,44 @@ export class ConjureSword extends Spell {
       return;
     }
 
-    this.sendMessage(caster, { message: 'You channel magical energy into the form of a sword.' });
+    const weaponType = spellCastArgs.originalArgs?.stringArgs ?? 'Longsword';
 
-    const item = this.game.itemCreator.getSimpleItem('Conjured Longsword');
+    const item = this.game.itemCreator.getSimpleItem(`Conjured ${weaponType}`);
+    const { itemClass, twoHanded, canShoot } = this.game.itemHelper.getItemProperties(item, ['itemClass', 'twoHanded', 'canShoot']);
+    if (!item || !WeaponClasses.includes(itemClass as WeaponClass) || itemClass === ItemClass.Shield) {
+      this.sendMessage(caster, { message: 'The ether churns at your request.' });
+      return;
+    }
+
+    this.sendMessage(caster, { message: `You channel the ether into the form of a ${weaponType.toLowerCase()}.` });
+    console.log(item);
 
     const skill = Math.max(spellCastArgs.potency, this.game.characterHelper.getSkillLevel(caster, Skill.Conjuration) + 1);
 
     item.mods.destroyOnDrop = true;
-    item.mods.offhand = true;
     item.mods.tier = Math.floor(skill / 4) + 1;
+
+    if (!twoHanded && !canShoot) {
+      item.mods.offhand = true;
+    }
 
     item.mods.stats = {};
     item.mods.stats[Stat.WeaponArmorClass] = Math.floor(skill / 6);
     item.mods.stats[Stat.Offense] = Math.floor(skill / 4);
     item.mods.stats[Stat.Defense] = Math.floor(skill / 4);
     item.mods.stats[Stat.Accuracy] = Math.floor(skill / 5);
+
+    if (twoHanded) {
+      item.mods.stats[Stat.WeaponArmorClass] = Math.floor(skill / 5);
+      item.mods.stats[Stat.Accuracy] = Math.floor(skill / 7);
+      item.mods.stats[Stat.Offense] = Math.floor(skill / 3);
+      item.mods.stats[Stat.Defense] = Math.floor(skill / 5);
+    }
+
+    if (canShoot) {
+      item.mods.stats[Stat.Defense] = 0;
+      item.mods.stats[Stat.Accuracy] = Math.floor(skill / 4);
+    }
 
     this.game.itemHelper.setOwner(caster as IPlayer, item);
 
