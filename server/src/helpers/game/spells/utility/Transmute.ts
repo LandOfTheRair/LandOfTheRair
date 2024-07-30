8ce4fa308d482d8968dc207ded5b158855a4f1a0
Kyle Kemp <kyle@seiyria.com>
2024-07-30 11:36:03 -0500

    fix(transmute): transmute base potency 30 -> 10, phil stone 20 -> 10

--- server/src/helpers/game/spells/utility/Transmute.ts
+++ server/src/helpers/game/spells/utility/Transmute.ts
@@ -1,15 +1,36 @@
-import { ICharacter, IGroundItem, ItemClass, Skill, SpellCastArgs } from '../../../../interfaces';
+import {
+  ICharacter,
+  IGroundItem,
+  ItemClass,
+  Skill,
+  SpellCastArgs,
+} from '../../../../interfaces';
 import { Spell } from '../../../../models/world/Spell';
 
 export class Transmute extends Spell {
-
   override getPotency(caster: ICharacter | null) {
-    return caster ? this.game.characterHelper.getSkillLevel(caster, Skill.Thievery) + 30 : 10;
+    return caster
+      ? this.game.characterHelper.getSkillLevel(caster, Skill.Thievery) + 10
+      : 10;
   }
 
-  override cast(caster: ICharacter | null, target: ICharacter | null, spellCastArgs: SpellCastArgs): void {
-    const center = target ? target : { x: spellCastArgs.x ?? 0, y: spellCastArgs.y ?? 0, map: spellCastArgs.map ?? '' };
-    const potency = spellCastArgs.potency + (caster ? this.game.traitHelper.traitLevelValue(caster, 'PhilosophersStone') : 0);
+  override cast(
+    caster: ICharacter | null,
+    target: ICharacter | null,
+    spellCastArgs: SpellCastArgs,
+  ): void {
+    const baseCenter = {
+      x: spellCastArgs.x ?? 0,
+      y: spellCastArgs.y ?? 0,
+      map: spellCastArgs.map ?? '',
+    };
+
+    const center = target ? target : baseCenter;
+    const potency =
+      spellCastArgs.potency +
+      (caster
+        ? this.game.traitHelper.traitLevelValue(caster, 'PhilosophersStone')
+        : 0);
 
     const mapData = this.game.worldManager.getMap(center.map);
     if (!mapData) return;
@@ -19,38 +40,51 @@ export class Transmute extends Spell {
     const removeItems: IGroundItem[] = [];
     let totalGold = 0;
 
-    Object.keys(items).forEach(itemClass => {
-      if (itemClass === ItemClass.Coin
-      || itemClass === ItemClass.Corpse) return;
+    Object.keys(items).forEach((itemClass) => {
+      if (itemClass === ItemClass.Coin || itemClass === ItemClass.Corpse) {
+        return;
+      }
 
       items[itemClass].forEach((groundItem: IGroundItem) => {
         if (groundItem.item.mods.owner) return;
 
-        const ounces = this.game.itemHelper.getItemProperty(groundItem.item, 'ounces');
+        const ounces = this.game.itemHelper.getItemProperty(
+          groundItem.item,
+          'ounces',
+        );
         if (itemClass === ItemClass.Bottle && ounces > 0) return;
 
-        const quality = this.game.itemHelper.getItemProperty(groundItem.item, 'quality');
+        const quality = this.game.itemHelper.getItemProperty(
+          groundItem.item,
+          'quality',
+        );
         if (quality > 0) return;
 
         removeItems.push(groundItem);
 
-        const value = this.game.inventoryHelper.itemValue(caster, groundItem.item);
-        totalGold += (value * (groundItem.count ?? 1));
+        const value = this.game.inventoryHelper.itemValue(
+          caster,
+          groundItem.item,
+        );
+        totalGold += value * (groundItem.count ?? 1);
       });
     });
 
     if (caster && (totalGold === 0 || removeItems.length === 0)) {
-      this.sendMessage(caster, { message: 'You do not see any items here to transmute.' });
+      this.sendMessage(caster, {
+        message: 'You do not see any items here to transmute.',
+      });
       return;
     }
 
-    totalGold *= (potency / 100);
+    totalGold *= potency / 100;
 
     const goldItem = this.game.itemCreator.getGold(totalGold);
     mapData.state.removeItemsFromGround(center.x, center.y, removeItems);
     mapData.state.addItemToGround(center.x, center.y, goldItem);
 
-    this.game.messageHelper.sendLogMessageToRadius(center, 4, { message: 'You hear the clinking of coins.' });
+    this.game.messageHelper.sendLogMessageToRadius(center, 4, {
+      message: 'You hear the clinking of coins.',
+    });
   }
-
 }
