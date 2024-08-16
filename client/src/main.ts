72612000147f36783908efd2950e2e21caaddbdd
Kyle Kemp <kyle@seiyria.com>
2024-08-16 15:43:51 -0500

    fix manifest link

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,14 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+console.log(environment.production);
+
 if (environment.production) {
+  console.log('production!!!');
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .then(() => {
     if ('serviceWorker' in navigator && environment.production) {
       navigator.serviceWorker.register('./ngsw-worker.js');
     }
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
