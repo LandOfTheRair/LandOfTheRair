c99f92333d0a93830bae191fcc282270c13040ef
Kyle Kemp <kyle@seiyria.com>
2024-08-26 14:57:24 -0500

    fix(spellshield): spellshield will round given saving throw value

--- server/src/helpers/game/effects/spell/buff/Spellshield.ts
+++ server/src/helpers/game/effects/spell/buff/Spellshield.ts
@@ -2,9 +2,9 @@ import { ICharacter, IStatusEffect, Stat } from '../../../../../interfaces';
 import { Effect } from '../../../../../models';
 
 export class Spellshield extends Effect {
-
   public override create(char: ICharacter, effect: IStatusEffect) {
-    effect.effectInfo.statChanges = { [Stat.SavingThrow]: effect.effectInfo.potency };
+    effect.effectInfo.statChanges = {
+      [Stat.SavingThrow]: Math.floor(effect.effectInfo.potency),
+    };
   }
-
 }
