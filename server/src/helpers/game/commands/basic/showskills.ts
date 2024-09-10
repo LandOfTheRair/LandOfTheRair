bbabd9ae820e96f4da74ffb73a6d06ab2b48690f
Kyle J. Kemp <kyle@seiyria.com>
2024-09-10 11:18:03 -0500

    feat(command): show skills now has skill name too

--- server/src/helpers/game/commands/basic/showskills.ts
+++ server/src/helpers/game/commands/basic/showskills.ts
@@ -1,21 +1,46 @@
-import { Allegiance, IMacroCommandArgs, IPlayer, MessageType, Skill } from '../../../../interfaces';
+import {
+  Allegiance,
+  IMacroCommandArgs,
+  IPlayer,
+  MessageType,
+  Skill,
+} from '../../../../interfaces';
 import { MacroCommand } from '../../../../models/macro';
 
 export class ShowSkills extends MacroCommand {
-
   override aliases = ['show skills'];
   override canBeInstant = false;
   override canBeFast = true;
 
   override execute(player: IPlayer, args: IMacroCommandArgs) {
     let message = `You are ${player.name}, the ${player.alignment} level ${player.level} ${player.baseClass}.`;
-    this.game.messageHelper.sendLogMessageToPlayer(player, { message, sfx: undefined }, [MessageType.Description]);
+    this.game.messageHelper.sendLogMessageToPlayer(
+      player,
+      { message, sfx: undefined },
+      [MessageType.Description],
+    );
     message = `Your allegiance lies with ${player.allegiance === Allegiance.None ? 'no one' : `the ${player.allegiance}`}.`;
-    this.game.messageHelper.sendLogMessageToPlayer(player, { message, sfx: undefined }, [MessageType.Description]);
+    this.game.messageHelper.sendLogMessageToPlayer(
+      player,
+      { message, sfx: undefined },
+      [MessageType.Description],
+    );
 
-    Object.keys(player.skills).forEach(key => {
-      message = `Your ${key.toUpperCase()} skill level is ${this.game.characterHelper.getSkillLevel(player, key as Skill)}.`;
-      this.game.messageHelper.sendLogMessageToPlayer(player, { message, sfx: undefined }, [MessageType.Description]);
+    Object.keys(player.skills).forEach((key) => {
+      const skillLevel = this.game.characterHelper.getSkillLevel(
+        player,
+        key as Skill,
+      );
+      const skillName = this.game.contentManager.getSkillDescription(
+        key as Skill,
+        skillLevel,
+      );
+      message = `Your ${key.toUpperCase()} skill level is ${skillLevel} (${skillName}).`;
+      this.game.messageHelper.sendLogMessageToPlayer(
+        player,
+        { message, sfx: undefined },
+        [MessageType.Description],
+      );
     });
   }
 }
