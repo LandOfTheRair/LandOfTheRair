70512dede6a799a9fba4806bf7a67783dcf40f1f
Kyle J. Kemp <kyle@seiyria.com>
2024-09-24 14:26:25 -0500

    fix(core): forgot password should not send an email if there is no account

--- server/src/helpers/account/EmailHelper.ts
+++ server/src/helpers/account/EmailHelper.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from 'injection-js';
 import { random } from 'lodash';
 import nodemailer from 'nodemailer';
@@ -9,12 +8,14 @@ import { BaseService } from '../../models/BaseService';
 
 @Injectable()
 export class EmailHelper extends BaseService {
-
   private transport;
 
   public init() {
     if (!process.env.SMTP_EMAIL || !process.env.SMTP_PASSWORD) {
-      this.game.logger.log('Email', 'No email or password set, skipping SMTP...');
+      this.game.logger.log(
+        'Email',
+        'No email or password set, skipping SMTP...',
+      );
       return;
     }
 
@@ -22,19 +23,23 @@ export class EmailHelper extends BaseService {
       service: process.env.SMTP_SERVICE || 'gmail',
       auth: {
         user: process.env.SMTP_EMAIL,
-        pass: process.env.SMTP_PASSWORD
-      }
+        pass: process.env.SMTP_PASSWORD,
+      },
     });
   }
 
   public async requestVerificationCode(account: Account): Promise<void> {
     if (!this.transport) throw new Error('SMTP not configured.');
 
-    const hours = this.game.contentManager.getGameSetting('auth', 'verificationHourExpiration') ?? 1;
+    const hours =
+      this.game.contentManager.getGameSetting(
+        'auth',
+        'verificationHourExpiration',
+      ) ?? 1;
 
     const code = `${random(100000, 999999)}`;
     account.verificationCode = code;
-    account.verificationExpiration = Date.now() + (3600 * hours * 1000);
+    account.verificationExpiration = Date.now() + 3600 * hours * 1000;
 
     const mail = {
       from: 'help@rair.land',
@@ -48,7 +53,7 @@ export class EmailHelper extends BaseService {
       This code will be valid for ${hours} hour(s).
 
       If you did not request this, please reach out to support@rair.land.
-      `
+      `,
     };
 
     try {
@@ -59,25 +64,36 @@ export class EmailHelper extends BaseService {
     }
   }
 
-  public async requestTemporaryPassword(emailOrUsername: string): Promise<void> {
-
+  public async requestTemporaryPassword(
+    emailOrUsername: string,
+  ): Promise<void> {
     let email = '';
     if (emailOrUsername.includes('@')) email = emailOrUsername;
 
     if (!email) {
       const account = await this.game.accountDB.getAccount(emailOrUsername);
-      if (!account) throw new Error('Account username specified, but not found.');
+      if (!account) {
+        throw new Error('Account username specified, but not found.');
+      }
       email = account.email;
     }
 
-    const finalAccountUsername = await this.game.accountDB.getAccountUsernameForEmail(email);
+    const finalAccountUsername =
+      await this.game.accountDB.getAccountUsernameForEmail(email);
+    if (!finalAccountUsername) {
+      throw new Error(
+        'That email does not have an account associated with it.',
+      );
+    }
 
     const code = uuid().split('-').join('');
 
     this.game.accountDB.setTemporaryPassword(email, code);
 
     if (!this.transport) {
-      throw new Error(`The mail server is not configured. Temporary password for email ${email} set to "${code}" (if email exists).`);
+      throw new Error(
+        `The mail server is not configured. Temporary password for email ${email} set to "${code}" (if email exists).`,
+      );
     }
 
     const mail = {
@@ -94,7 +110,7 @@ export class EmailHelper extends BaseService {
       When you log in, reset your password immediately.
 
       If you did not request this, please reach out to support@rair.land.
-      `
+      `,
     };
 
     try {
@@ -104,5 +120,4 @@ export class EmailHelper extends BaseService {
       throw e;
     }
   }
-
 }
