e816f567a82eeebae18c2f15802ec65246e986bb
Kyle J. Kemp <kyle@seiyria.com>
2024-09-25 16:14:13 -0500

    fix(spell): songs will now give a 10s buff rather than 5, to help prevent lapses

--- server/src/helpers/game/effects/spell/song/PowerBalladSong.ts
+++ server/src/helpers/game/effects/spell/song/PowerBalladSong.ts
@@ -1,11 +1,19 @@
-import { ICharacter, IStatusEffect, Skill, Stat } from '../../../../../interfaces';
+import {
+  ICharacter,
+  IStatusEffect,
+  Skill,
+  Stat,
+} from '../../../../../interfaces';
 import { Song } from './Song';
 
 export class PowerBalladSong extends Song {
-
   public override create(char: ICharacter, effect: IStatusEffect) {
-
-    effect.effectInfo.potency = Math.floor(Math.max(1, this.game.characterHelper.getSkillLevel(char, Skill.Thievery) / 6));
+    effect.effectInfo.potency = Math.floor(
+      Math.max(
+        1,
+        this.game.characterHelper.getSkillLevel(char, Skill.Thievery) / 6,
+      ),
+    );
     effect.effectInfo.statChanges = {
       [Stat.STR]: effect.effectInfo.potency,
       [Stat.INT]: effect.effectInfo.potency,
@@ -18,33 +26,35 @@ export class PowerBalladSong extends Song {
   override tick(char: ICharacter, effect: IStatusEffect) {
     super.tick(char, effect);
 
-    if (((effect.effectInfo.currentTick ?? 0) % 5) === 0) {
-      this.game.worldManager.getMapStateForCharacter(char)?.getAllAlliesInRange(char, 4).forEach(ally => {
-        if (char === ally) return;
+    if ((effect.effectInfo.currentTick ?? 0) % 5 === 0) {
+      this.game.worldManager
+        .getMapStateForCharacter(char)
+        ?.getAllAlliesInRange(char, 4)
+        .forEach((ally) => {
+          if (char === ally) return;
 
-        if (!this.game.effectHelper.hasEffect(ally, 'TargetSong')) {
-          this.sendMessage(ally, { message: 'You hear a ballad of power!' });
-        }
-
-        this.game.effectHelper.addEffect(ally, char, 'TargetSong', {
-          effect: {
-            duration: 5,
-            extra: {
-              hideTicks: true,
-              effectIcon: 'music-spell',
-              tooltipName: 'Song',
-              tooltipColor: '#a00',
-              tooltip: `+${effect.effectInfo.potency} STR/INT/WIS`,
-              statChanges: {
-                [Stat.STR]: effect.effectInfo.potency,
-                [Stat.INT]: effect.effectInfo.potency,
-                [Stat.WIS]: effect.effectInfo.potency,
-              }
-            }
+          if (!this.game.effectHelper.hasEffect(ally, 'TargetSong')) {
+            this.sendMessage(ally, { message: 'You hear a ballad of power!' });
           }
+
+          this.game.effectHelper.addEffect(ally, char, 'TargetSong', {
+            effect: {
+              duration: 10,
+              extra: {
+                hideTicks: true,
+                effectIcon: 'music-spell',
+                tooltipName: 'Song',
+                tooltipColor: '#a00',
+                tooltip: `+${effect.effectInfo.potency} STR/INT/WIS`,
+                statChanges: {
+                  [Stat.STR]: effect.effectInfo.potency,
+                  [Stat.INT]: effect.effectInfo.potency,
+                  [Stat.WIS]: effect.effectInfo.potency,
+                },
+              },
+            },
+          });
         });
-      });
     }
   }
-
 }
