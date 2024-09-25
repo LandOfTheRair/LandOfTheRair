26a322725ba59b8b65f9227c8ab31e3dfafc101b
Kyle J. Kemp <kyle@seiyria.com>
2024-09-25 15:48:38 -0500

    fix(spell): remove useless code for snare

--- server/src/helpers/game/spells/debuff/Snare.ts
+++ server/src/helpers/game/spells/debuff/Snare.ts
@@ -1,18 +1,9 @@
-import { ICharacter, SpellCastArgs, Stat } from '../../../../interfaces';
+import { ICharacter } from '../../../../interfaces';
 import { Spell } from '../../../../models/world/Spell';
 
 export class Snare extends Spell {
-
   override getPotency(caster: ICharacter | null) {
     if (!caster) return 2;
     return this.game.traitHelper.traitLevelValue(caster, 'Roots') ? 4 : 2;
   }
-
-  override getDuration(caster: ICharacter | null) {
-    return caster ? Math.floor(this.game.characterHelper.getStat(caster, Stat.WIS) / 2) : 3;
-  }
-
-  override cast(caster: ICharacter | null, target: ICharacter | null, spellCastArgs: SpellCastArgs): void {
-  }
-
 }
