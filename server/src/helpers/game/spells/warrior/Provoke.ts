46cde3ab8cdd4788888536fd0b845c7c8baec7bf
Kyle J. Kemp <kyle@seiyria.com>
2024-09-26 11:10:48 -0500

    feat(spell): provoke will force retarget players

--- server/src/helpers/game/spells/warrior/Provoke.ts
+++ server/src/helpers/game/spells/warrior/Provoke.ts
@@ -1,14 +1,36 @@
-import { ICharacter, SpellCastArgs } from '../../../../interfaces';
+import { ICharacter, MessageType, SpellCastArgs } from '../../../../interfaces';
 import { Spell } from '../../../../models/world/Spell';
 
 export class Provoke extends Spell {
-
-  override cast(caster: ICharacter | null, target: ICharacter | null, spellCastArgs: SpellCastArgs): void {
+  override cast(
+    caster: ICharacter | null,
+    target: ICharacter | null,
+    spellCastArgs: SpellCastArgs,
+  ): void {
     if (!caster || !target) return;
 
-    this.game.messageHelper.sendSimpleMessage(caster, `You provoked ${target.name}!`);
-    this.game.messageHelper.sendSimpleMessage(target, `${caster.name} provoked you!`);
-    this.game.characterHelper.addAgro(caster, target, this.game.spellManager.getPotency(caster, target, spellCastArgs.spellData));
-  }
+    this.game.messageHelper.sendSimpleMessage(
+      caster,
+      `You provoked ${target.name}!`,
+    );
 
+    this.game.messageHelper.sendLogMessageToPlayer(
+      target,
+      {
+        message: `${caster.name} provoked you!`,
+        setTarget: caster.uuid,
+      },
+      [MessageType.Miscellaneous],
+    );
+
+    this.game.characterHelper.addAgro(
+      caster,
+      target,
+      this.game.spellManager.getPotency(
+        caster,
+        target,
+        spellCastArgs.spellData,
+      ),
+    );
+  }
 }
