2a4f163c6264097fba2afa49b1d926f49251d1ad
Kyle J. Kemp <kyle@seiyria.com>
2024-10-03 12:12:11 -0500

    fix(spell): vitalessence hp value down by 40%

--- server/src/helpers/game/effects/spell/buff/VitalEssence.ts
+++ server/src/helpers/game/effects/spell/buff/VitalEssence.ts
@@ -11,7 +11,7 @@ export class VitalEssence extends Effect {
   public override create(char: ICharacter, effect: IStatusEffect) {
     effect.effectInfo.potency = effect.effectInfo.potency ?? 0;
 
-    const hpBoost = effect.effectInfo.potency * 25;
+    const hpBoost = effect.effectInfo.potency * 15;
     const acBoost = Math.floor(effect.effectInfo.potency / 2);
 
     effect.effectInfo.statChanges = {
