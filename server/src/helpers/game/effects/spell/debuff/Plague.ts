1bee490ab4adb0bf656dadfb1cb51999bbbbda70
Kyle J. Kemp <kyle@seiyria.com>
2024-10-03 12:43:51 -0500

    fix(spell): plague spreading should never spread with a duration of -1 on accident

--- server/src/helpers/game/effects/spell/debuff/Plague.ts
+++ server/src/helpers/game/effects/spell/debuff/Plague.ts
@@ -1,23 +1,31 @@
-
 import { sample } from 'lodash';
 
-import { DamageClass, ICharacter, IPlayer, IStatusEffect } from '../../../../../interfaces';
+import {
+  DamageClass,
+  ICharacter,
+  IPlayer,
+  IStatusEffect,
+} from '../../../../../interfaces';
 import { Effect } from '../../../../../models';
 
 export class Plague extends Effect {
-
   public override create(char: ICharacter, effect: IStatusEffect) {
-
     if (effect.sourceUUID) {
       const mapState = this.game.worldManager.getMap(char.map)?.state;
       const caster = mapState?.getCharacterByUUID(effect.sourceUUID);
 
       if (caster) {
-        effect.effectInfo.isContagious = this.game.traitHelper.hasLearnedTrait(caster as IPlayer, 'ContagiousPlague');
+        effect.effectInfo.isContagious = this.game.traitHelper.hasLearnedTrait(
+          caster as IPlayer,
+          'ContagiousPlague',
+        );
 
         // pandemic lets us spread immediately
         if (!effect.effectInfo.isSpreadEffect) {
-          const numSpreads = this.game.traitHelper.traitLevelValue(caster, 'Pandemic');
+          const numSpreads = this.game.traitHelper.traitLevelValue(
+            caster,
+            'Pandemic',
+          );
           for (let i = 0; i < numSpreads; i++) {
             this.spread(char, effect, caster);
           }
@@ -38,41 +46,56 @@ export class Plague extends Effect {
     this.game.combatHelper.magicalAttack(caster, char, {
       damage: effect.effectInfo.potency,
       damageClass: DamageClass.Disease,
-      defMsg: 'You are plagued!'
+      defMsg: 'You are plagued!',
     });
 
     // spread the contagion
     const curTick = effect.effectInfo.currentTick ?? 0;
-    if (curTick > 3 && effect.effectInfo.isContagious && (curTick % 3) === 0) {
+    if (curTick > 3 && effect.effectInfo.isContagious && curTick % 3 === 0) {
       this.spread(char, effect, caster);
     }
   }
 
-  private spread(char: ICharacter, effect: IStatusEffect, caster: ICharacter | null) {
+  private spread(
+    char: ICharacter,
+    effect: IStatusEffect,
+    caster: ICharacter | null,
+  ) {
     const mapState = this.game.worldManager.getMap(char.map)?.state;
     if (!mapState) return;
 
-    const nearby = mapState.getAllInRange(char, 1).filter(x => x !== char
-      && caster ? this.game.targettingHelper.checkTargetForHostility(caster, x) : true
-      && !this.game.effectHelper.hasEffect(x, 'Plague'));
+    const nearby = mapState
+      .getAllInRange(char, 1)
+      .filter((x) =>
+        x !== char && caster
+          ? this.game.targettingHelper.checkTargetForHostility(caster, x)
+          : true && !this.game.effectHelper.hasEffect(x, 'Plague'),
+      );
 
     if (nearby.length === 0) return;
 
     const spreadTo = sample(nearby);
     if (spreadTo) {
-      this.game.effectHelper.addEffect(spreadTo, caster ?? 'somebody', 'Plague', {
-        effectMeta: {
-          effectRef: 'Plague'
+      this.game.effectHelper.addEffect(
+        spreadTo,
+        caster ?? 'somebody',
+        'Plague',
+        {
+          effectMeta: {
+            effectRef: 'Plague',
+          },
+          effect: {
+            duration: Math.max(
+              1,
+              Math.floor((effect.endsAt - Date.now()) / 1000),
+            ),
+            extra: {
+              ...effect.effectInfo,
+              isSpreadEffect: true,
+            },
+          },
         },
-        effect: {
-          duration: Math.floor((effect.endsAt - Date.now()) / 1000),
-          extra: {
-            ...effect.effectInfo,
-            isSpreadEffect: true
-          }
-        }
-      });
+      );
     }
   }
-
 }
