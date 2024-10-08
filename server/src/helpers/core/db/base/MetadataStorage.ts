4d734115357027c59cf9573d04e9da89f2984ffc
Kyle J. Kemp <kyle@seiyria.com>
2024-10-08 13:53:48 -0500

    fix(core): createdAt now actually.. works.. whoops!

--- server/src/helpers/core/db/base/MetadataStorage.ts
+++ server/src/helpers/core/db/base/MetadataStorage.ts
@@ -1,11 +1,15 @@
-
 import { isUndefined, kebabCase, pickBy } from 'lodash';
 
 export class MetadataStorage {
-
   private static collectionNames: Record<string, string> = {};
-  private static hiddenPropertiesByEntity: Record<string, Record<string, boolean>> = {};
-  private static persistPropertiesByEntity: Record<string, Record<string, boolean>> = {};
+  private static hiddenPropertiesByEntity: Record<
+    string,
+    Record<string, boolean>
+  > = {};
+  private static persistPropertiesByEntity: Record<
+    string,
+    Record<string, boolean>
+  > = {};
 
   private static getTypeForEntity(entity): string {
     return entity.prototype?.constructor?.name || entity.constructor?.name;
@@ -13,7 +17,9 @@ export class MetadataStorage {
 
   static getCollectionForEntity(entity): string {
     const entityName = this.getTypeForEntity(entity);
-    if (!entityName) throw new Error(`Error getting collection for entity ${entity.name}`);
+    if (!entityName) {
+      throw new Error(`Error getting collection for entity ${entity.name}`);
+    }
 
     return this.collectionNames[entityName];
   }
@@ -28,15 +34,19 @@ export class MetadataStorage {
     const entityName = this.getTypeForEntity(entity);
     if (!entityName) return;
 
-    this.hiddenPropertiesByEntity[entityName] = this.hiddenPropertiesByEntity[entityName] || {};
-    this.persistPropertiesByEntity[entityName] = this.persistPropertiesByEntity[entityName] || {};
+    this.hiddenPropertiesByEntity[entityName] =
+      this.hiddenPropertiesByEntity[entityName] || {};
+    this.persistPropertiesByEntity[entityName] =
+      this.persistPropertiesByEntity[entityName] || {};
 
     if (!isUndefined(propertyMetadata.hidden)) {
-      this.hiddenPropertiesByEntity[entityName][propName] = propertyMetadata.hidden;
+      this.hiddenPropertiesByEntity[entityName][propName] =
+        propertyMetadata.hidden;
     }
 
     if (!isUndefined(propertyMetadata.persist)) {
-      this.persistPropertiesByEntity[entityName][propName] = propertyMetadata.persist;
+      this.persistPropertiesByEntity[entityName][propName] =
+        propertyMetadata.persist;
     }
   }
 
@@ -54,7 +64,10 @@ export class MetadataStorage {
     const entityName = this.getTypeForEntity(object);
     if (!entityName) return null;
 
-    return pickBy(object, (value, key) => this.persistPropertiesByEntity[entityName][key]);
+    return pickBy(
+      object,
+      (value, key) =>
+        this.persistPropertiesByEntity[entityName][key] || key === 'createdAt',
+    );
   }
-
 }
