3063c4afa5e8df958db30af2a24f8fa459676d43
Kyle J. Kemp <kyle@seiyria.com>
2024-10-14 06:33:32 -0500

    feat(gm): eval command now uses modal sendback

--- server/src/helpers/game/commands/gm/eval.ts
+++ server/src/helpers/game/commands/gm/eval.ts
@@ -1,22 +1,52 @@
-import { IMacroCommandArgs, IPlayer } from '../../../../interfaces';
+import {
+  GameServerResponse,
+  IMacroCommandArgs,
+  IPlayer,
+} from '../../../../interfaces';
 import { MacroCommand } from '../../../../models/macro';
 import { Game } from '../../../core/Game';
 
 export class GMEval extends MacroCommand {
-
   override aliases = ['@eval', '@exec', '@evalj'];
   override isGMCommand = true;
   override canBeInstant = false;
   override canBeFast = false;
 
   override execute(player: IPlayer, args: IMacroCommandArgs) {
-
     try {
-      const result = this.myEval.call({}, args.stringArgs, { game: this.game, player });
+      const result = this.myEval.call({}, args.stringArgs, {
+        game: this.game,
+        player,
+      });
       switch (args.calledAlias) {
-        case '@exec': this.sendMessage(player, 'Command Executed'); break;
-        case '@evalj': this.sendMessage(player, `Command Result<br>${JSON.stringify(result, null, 2).replace(/\n/g, '<br>')}`); break;
-        default: this.sendMessage(player, `Command Result<br>${result}`); break;
+        case '@exec': {
+          this.sendMessage(player, 'Command Executed');
+          break;
+        }
+
+        case '@evalj': {
+          this.sendMessage(
+            player,
+            `Command Result<br>${JSON.stringify(result, null, 2).replace(/\n/g, '<br>')}`,
+          );
+
+          args.callbacks.emit({
+            type: GameServerResponse.SendAlert,
+            title: `Command Result`,
+            content: JSON.stringify(result, null, 2),
+          });
+          break;
+        }
+
+        default: {
+          this.sendMessage(player, `Command Result<br>${result}`);
+          args.callbacks.emit({
+            type: GameServerResponse.SendAlert,
+            title: `Command Result`,
+            content: result,
+          });
+          break;
+        }
       }
     } catch (error) {
       this.sendMessage(player, `Eval Error: ${error}`);
@@ -40,8 +70,9 @@ export class GMEval extends MacroCommand {
     const game = context.game;
     const json = (obj: any) => JSON.stringify(obj, null, 2);
     const props = (obj: any) => Object.getOwnPropertyNames(obj);
-    const players = () => ((context.game) as Game).playerManager.getAllPlayers();
-    const user = (username: string) => ((context.game) as Game).playerManager.getPlayerByUsername(username);
+    const players = () => (context.game as Game).playerManager.getAllPlayers();
+    const user = (username: string) =>
+      (context.game as Game).playerManager.getPlayerByUsername(username);
 
     // eslint-disable-next-line no-eval
     return eval(script);
