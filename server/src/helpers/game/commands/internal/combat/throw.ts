bd53d124e661368b7e185701a7fe73ff4af65830
Kyle J. Kemp <kyle@seiyria.com>
2025-09-30 08:56:16 -0500

    fix(talent): bouncing throws should not hit other players, hopefully fixed
    closes #531

--- server/src/helpers/game/commands/internal/combat/throw.ts
+++ server/src/helpers/game/commands/internal/combat/throw.ts
@@ -1,11 +1,16 @@
-
 import { sample } from 'lodash';
 
-import { distanceFrom, ICharacter, IMacroCommandArgs, IPlayer, ItemSlot, PhysicalAttackArgs } from '../../../../../interfaces';
+import {
+  distanceFrom,
+  ICharacter,
+  IMacroCommandArgs,
+  IPlayer,
+  ItemSlot,
+  PhysicalAttackArgs,
+} from '../../../../../interfaces';
 import { SkillCommand } from '../../../../../models/macro';
 
 export class ThrowCommand extends SkillCommand {
-
   override aliases = ['t', 'throw'];
 
   override range(): number {
@@ -15,41 +20,57 @@ export class ThrowCommand extends SkillCommand {
   override execute(player: IPlayer, args: IMacroCommandArgs) {
     const [hand, char] = args.arrayArgs;
 
-    const target = this.game.targettingHelper.getFirstPossibleTargetInViewRange(player, char);
+    const target = this.game.targettingHelper.getFirstPossibleTargetInViewRange(
+      player,
+      char,
+    );
     if (!target) return this.youDontSeeThatPerson(player, args.stringArgs);
 
     if (target === player) return;
 
-    if (distanceFrom(player, target) > this.range()) return this.sendMessage(player, 'That target is too far away!');
+    if (distanceFrom(player, target) > this.range()) {
+      return this.sendMessage(player, 'That target is too far away!');
+    }
 
     const itemSlot = hand === 'left' ? ItemSlot.LeftHand : ItemSlot.RightHand;
-    if (!player.items.equipment[itemSlot]) return this.sendMessage(player, 'You do not have anything to throw in that hand!');
+    if (!player.items.equipment[itemSlot]) {
+      return this.sendMessage(
+        player,
+        'You do not have anything to throw in that hand!',
+      );
+    }
 
     this.use(player, target, { throwHand: itemSlot });
   }
 
-  override use(user: ICharacter, target: ICharacter, opts: PhysicalAttackArgs = {}): void {
+  override use(
+    user: ICharacter,
+    target: ICharacter,
+    opts: PhysicalAttackArgs = {},
+  ): void {
     opts.isThrow = true;
     opts.throwHand = opts.throwHand || ItemSlot.RightHand;
     opts.attackRange = this.range();
 
-    const numThrows = 1 + this.game.traitHelper.traitLevelValue(user, 'Multithrow');
+    const numThrows =
+      1 + this.game.traitHelper.traitLevelValue(user, 'Multithrow');
     for (let i = 0; i < numThrows; i++) {
       this.game.combatHelper.physicalAttack(user, target, opts);
 
-      if (user && target && this.game.traitHelper.rollTraitValue(user, 'BouncingThrows')) {
-        const state = this.game.worldManager.getMapStateForCharacter(user);
-        if (!state) return;
-
-        const nearby = state.getAllHostilesInRange(user, 4).filter(x => x !== target);
+      if (
+        user &&
+        target &&
+        this.game.traitHelper.rollTraitValue(user, 'BouncingThrows')
+      ) {
+        const nearby = this.game.targettingHelper
+          .getPossibleAOETargets(user, user, 4)
+          .filter((x) => x !== target);
 
         const bounceTo = sample(nearby);
         if (bounceTo) {
           this.game.combatHelper.physicalAttack(user, bounceTo, opts);
         }
-
       }
     }
   }
-
 }
