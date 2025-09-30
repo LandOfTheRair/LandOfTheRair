306af167b9e772be323dd3c8c9c15f881692c5e4
Kyle J. Kemp <kyle@seiyria.com>
2025-09-30 08:33:04 -0500

    feat(spell): gemsense will sense gems encrusted into items
    closes #525

--- server/src/helpers/game/spells/utility/Gemsense.ts
+++ server/src/helpers/game/spells/utility/Gemsense.ts
@@ -1,38 +1,58 @@
-import { GameServerResponse, gemTextFor, ICharacter, IPlayer, ItemClass, ItemSlot, SpellCastArgs } from '../../../../interfaces';
+import {
+  GameServerResponse,
+  gemTextFor,
+  ICharacter,
+  IPlayer,
+  ItemClass,
+  ItemSlot,
+  SpellCastArgs,
+} from '../../../../interfaces';
 import { Spell } from '../../../../models/world/Spell';
 
 export class Gemsense extends Spell {
-
-  override cast(caster: ICharacter | null, target: ICharacter | null, spellCastArgs: SpellCastArgs): void {
+  override cast(
+    caster: ICharacter | null,
+    target: ICharacter | null,
+    spellCastArgs: SpellCastArgs,
+  ): void {
     if (!caster) return;
 
     const rightHand = caster?.items.equipment[ItemSlot.RightHand];
     if (!rightHand) {
-      this.sendMessage(caster, { message: 'You do not have anything in your right hand!' });
+      this.sendMessage(caster, {
+        message: 'You do not have anything in your right hand!',
+      });
       return;
     }
 
-    const { itemClass } = this.game.itemHelper.getItemProperties(rightHand, ['itemClass']);
-
-    if (itemClass !== ItemClass.Gem) {
-      this.sendMessage(caster, { message: 'You do not have a gem item in your right hand!' });
-      return;
-    }
-
-    const identMsg = gemTextFor(
-      caster as IPlayer,
+    const { itemClass, encrustItem } = this.game.itemHelper.getItemProperties(
       rightHand,
-      this.game.itemHelper.getItemDefinition(rightHand.name)
+      ['itemClass', 'encrustItem'],
     );
+
+    if (!encrustItem && itemClass !== ItemClass.Gem) {
+      this.sendMessage(caster, {
+        message: 'You do not have a gem or encrusted item in your right hand!',
+      });
+      return;
+    }
+
+    const itemDef = this.game.itemHelper.getItemDefinition(
+      encrustItem || rightHand.name,
+    );
+
+    const identMsg = gemTextFor(caster as IPlayer, rightHand, itemDef);
 
     spellCastArgs.callbacks?.emit({
       type: GameServerResponse.SendAlert,
       title: 'Gemsense',
       content: identMsg,
-      extraData: { itemName: rightHand.name, displayItemSprite: this.game.itemHelper.getItemProperty(rightHand, 'sprite') },
+      extraData: {
+        itemName: rightHand.name,
+        displayItemSprite: itemDef.sprite,
+      },
     });
 
     this.sendMessage(caster, { message: identMsg });
   }
-
 }
