855cdd31af5d88d8df75f3d0664c2ad711b52a10
Kyle J. Kemp <kyle@seiyria.com>
2025-10-05 13:37:02 -0500

    feat(spell): antidote now dispels venom
    closes #575

--- server/src/helpers/game/spells/heals/Antidote.ts
+++ server/src/helpers/game/spells/heals/Antidote.ts
@@ -2,19 +2,27 @@ import { ICharacter, SpellCastArgs } from '../../../../interfaces';
 import { Spell } from '../../../../models/world/Spell';
 
 export class Antidote extends Spell {
-
-  override cast(caster: ICharacter | null, target: ICharacter | null, spellCastArgs: SpellCastArgs): void {
+  override cast(
+    caster: ICharacter | null,
+    target: ICharacter | null,
+    spellCastArgs: SpellCastArgs,
+  ): void {
     if (!target) return;
     const hasPoison = this.game.effectHelper.hasEffect(target, 'Poison');
     const hasDisease = this.game.effectHelper.hasEffect(target, 'Disease');
+    const hasVenom = this.game.effectHelper.hasEffect(target, 'Venom');
 
-    if (!hasPoison && !hasDisease && caster) {
-      this.sendMessage(caster, { message: `${target.name} is not poisoned or diseased.` });
+    if (!hasPoison && !hasDisease && !hasVenom && caster) {
+      this.sendMessage(caster, {
+        message: `${target.name} is not poisoned, diseased, or affected by venom.`,
+      });
       return;
     }
 
     if (hasPoison) this.game.effectHelper.removeEffectByName(target, 'Poison');
-    if (hasDisease) this.game.effectHelper.removeEffectByName(target, 'Disease');
+    if (hasDisease) {
+      this.game.effectHelper.removeEffectByName(target, 'Disease');
+    }
+    if (hasVenom) this.game.effectHelper.removeEffectByName(target, 'Venom');
   }
-
 }
