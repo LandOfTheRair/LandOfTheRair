618cafcf21623dd305d9f5d6f4dd7ac1244f7c0c
Kyle J. Kemp <kyle@seiyria.com>
2025-10-15 09:11:22 -0500

    feat(gm): improve force reboot to kick all players first

--- apps/server/src/http/forcereboot.ts
+++ apps/server/src/http/forcereboot.ts
@@ -1,9 +1,9 @@
 import { get } from 'lodash';
 
-import { GameServerEvent } from '@lotr/interfaces';
+import { GameServerEvent, GameServerResponse } from '@lotr/interfaces';
 
 export class ForceRebootRoute {
-  static setup(fastify: any, { sendToGame }) {
+  static setup(fastify: any, { sendToGame, broadcast }) {
     fastify.post('/reboot/force', async (req, res) => {
       if (process.env.NODE_ENV === 'production') {
         const secret = process.env.WEBHOOK_SECRET;
@@ -13,8 +13,28 @@ export class ForceRebootRoute {
       }
 
       sendToGame({
-        type: GameServerEvent.ForceReboot,
+        type: GameServerEvent.Announce,
+        message:
+          'The game will be rebooting shortly. All players will be kicked soon.',
       });
+
+      broadcast({
+        type: GameServerResponse.SendImportantNotification,
+        message:
+          'The game will be rebooting shortly. You will be kicked soon to ensure your data gets saved.',
+      });
+
+      setTimeout(() => {
+        sendToGame({
+          type: GameServerEvent.BlockAndKickAll,
+        });
+
+        setTimeout(() => {
+          sendToGame({
+            type: GameServerEvent.ForceReboot,
+          });
+        }, 5000);
+      }, 5000);
 
       res.send({});
     });
